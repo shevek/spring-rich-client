0e7a0a0beb51794a13e268ff1461cbd3d0737298
Oliver Hutchison <oliverh@affirmsoftware.com.au>
2004-07-16 00:05:55 +0000

    Added correct header
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@41 817809c7-9d0e-0410-b92d-a7ac8b8adc13

--- src/org/springframework/richclient/forms/JFormatedTextFieldValueSetter.java
+++ src/org/springframework/richclient/forms/JFormatedTextFieldValueSetter.java
@@ -1,9 +1,18 @@
 /*
- * Copyright 2004 (C) Our Community Pty. Ltd. All Rights Reserved
+ * Copyright 2002-2004 the original author or authors.
  * 
- * $Id$
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
-
 package org.springframework.richclient.forms;
 
 import java.beans.PropertyChangeEvent;
@@ -16,7 +25,6 @@ import org.springframework.rules.values.ValueModel;
 
 /**
  * @author oliverh
- * @version $Revision$ $Date$
  */
 public class JFormatedTextFieldValueSetter
 extends AbstractValueSetter
