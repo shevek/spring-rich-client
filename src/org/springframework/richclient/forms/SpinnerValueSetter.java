d368b1857d8f6d4c6d4e007a5ddac3ca9804d4be
Keith Donald <keith.donald@springsource.com>
2004-07-28 00:11:09 +0000

    copyright
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@78 817809c7-9d0e-0410-b92d-a7ac8b8adc13

--- src/org/springframework/richclient/forms/SpinnerValueSetter.java
+++ src/org/springframework/richclient/forms/SpinnerValueSetter.java
@@ -1,8 +1,17 @@
 /*
- * $Header: /usr/local/cvs/module/src/java/File.java,v 1.7 2004/01/16 22:23:11
- * keith Exp $ $Revision$ $Date$
+ * Copyright 2002-2004 the original author or authors.
  * 
- * Copyright Computer Science Innovations (CSI), 2004. All rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package org.springframework.richclient.forms;
 
