b6af25668c224c547a30b66283d716302de43421
Jim Moore <springrich@mooresoft.net>
2004-08-12 18:23:53 +0000

    Add the eventExceptionHandler property so clients can specify what class to use when handling exceptions that happen in the event thread
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@147 817809c7-9d0e-0410-b92d-a7ac8b8adc13

--- src/org/springframework/richclient/application/config/ApplicationAdvisor.java
+++ src/org/springframework/richclient/application/config/ApplicationAdvisor.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2002-2004 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -15,10 +15,11 @@
  */
 package org.springframework.richclient.application.config;
 
-import java.awt.Image;
+import java.awt.*;
+import java.util.Properties;
 
-import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.richclient.application.Application;
 import org.springframework.richclient.application.ApplicationInfo;
 import org.springframework.richclient.application.ApplicationWindow;
@@ -29,11 +30,11 @@ import org.springframework.richclient.progress.StatusBarCommandGroup;
 
 /**
  * @author Keith Donald
+ * @author Jim Moore
  */
-public abstract class ApplicationAdvisor {
+public abstract class ApplicationAdvisor implements InitializingBean {
     private static final String DEFAULT_APPLICATION_IMAGE_KEY = "applicationInfo.image";
-
-    private final Log logger = LogFactory.getLog(getClass());
+    private static final String EXCEPTION_HANDLER_KEY = "sun.awt.exception.handler";
 
     private Application application;
 
@@ -43,27 +44,52 @@ public abstract class ApplicationAdvisor {
 
     private boolean introShown;
 
-    static {
-        System
-                .getProperties()
-                .put(
-                        "sun.awt.exception.handler",
-                        "org.springframework.richclient.application.config.ApplicationAdvisor$EventExceptionHandler");
-    }
+    private Class eventExceptionHandler;
+
 
     /**
-     * Logs any event loop exception not caught; makes use of the hack Sun
-     * created to get around not having a good API for doing this. Doesn't work
-     * for JDK 1.1.
-     * 
-     * See java.awt.EventDispatchThread#handleException(Throwable)
+     * Logs any event loop exception not caught.
+     *
+     * @see ApplicationAdvisor#setEventExceptionHandler(Class)
      */
-    public static class EventExceptionHandler {
+    public static class DefaultEventExceptionHandler {
         public void handle(Throwable t) {
             LogFactory.getLog(ApplicationAdvisor.class)
                     .error(t.getMessage(), t);
         }
     }
+
+
+    public Class getEventExceptionHandler() {
+        if (this.eventExceptionHandler == null) {
+            this.eventExceptionHandler = DefaultEventExceptionHandler.class;
+        }
+        return this.eventExceptionHandler;
+    }
+
+
+    /**
+     * Sets the class to use to handle exceptions that happen in the
+     * {@link EventDispatchThread}.  The class must have a no-arg public
+     * constructor and a method "public void handle(Throwable)".
+     *
+     * @param eventExceptionHandler the class to use
+     *
+     * @see EventDispatchThread#handleException(Throwable)
+     */
+    public void setEventExceptionHandler(Class eventExceptionHandler) {
+        this.eventExceptionHandler = eventExceptionHandler;
+    }
+
+
+    public void afterPropertiesSet() throws Exception {
+        final Properties systemProperties = System.getProperties();
+        if (systemProperties.get(EXCEPTION_HANDLER_KEY) == null) {
+            systemProperties.put(EXCEPTION_HANDLER_KEY,
+                getEventExceptionHandler().getName());
+        }
+    }
+
 
     public void setApplicationInfo(ApplicationInfo info) {
         this.applicationInfo = info;
