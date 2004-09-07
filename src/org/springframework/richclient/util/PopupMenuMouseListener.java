096ef808f15e1484e4491cf717487212d19c3359
Oliver Hutchison <oliverh@affirmsoftware.com.au>
2004-09-07 04:32:49 +0000

    made more flexible and added some docs
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@200 817809c7-9d0e-0410-b92d-a7ac8b8adc13

--- src/org/springframework/richclient/util/PopupMenuMouseListener.java
+++ src/org/springframework/richclient/util/PopupMenuMouseListener.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2002-2004 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.richclient.util;
 
 import java.awt.event.MouseAdapter;
@@ -8,36 +23,74 @@ import javax.swing.JPopupMenu;
 import org.springframework.util.Assert;
 
 /**
+ * Convenient listener that will show a popup menu when it receives a popup
+ * trigger. 
+ * 
  * @author Keith Donald
+ * @author Oliver Hutchison
+ * @see java.awt.event.MouseEvent#isPopupTrigger
  */
 public class PopupMenuMouseListener extends MouseAdapter {
 
     private JPopupMenu popupMenu;
-    
+
+    /**
+     * Creates a new PopupMenuMouseListener.
+     * <p>
+     * NOTE: When using this constructor subclasses must overide the method
+     * getPopupMenu.
+     */
+    protected PopupMenuMouseListener() {
+    }
+
+    /**
+     * Creates a new PopupMenuMouseListener that will show the provided popup.
+     */
     public PopupMenuMouseListener(JPopupMenu popupMenu) {
         Assert.notNull(popupMenu);
         this.popupMenu = popupMenu;
     }
-    
-    
+
+    /**
+     * Called before the popup menu becomes visible. May veto the display of the
+     * popup by returning false.
+     */
+    protected boolean onAboutToShow(MouseEvent e) {
+        return true;
+    }
+
+    /**
+     * Called to display the popup menu.
+     */
+    protected void showPopupMenu(MouseEvent e) {
+        JPopupMenu popupToShow = getPopupMenu();
+        if (popupToShow == null) { return; }
+        popupToShow.show(e.getComponent(), e.getX(), e.getY());
+        popupToShow.setVisible(true);
+    }
+
+    /**
+     * Returns the popup menu to be displayed.
+     */
+    protected JPopupMenu getPopupMenu() {
+        if (popupMenu == null) { throw new UnsupportedOperationException(
+                "Method getPopupMenu must be overridden when default constructor is used."); }
+        return popupMenu;
+    }
+
     public void mousePressed(MouseEvent e) {
         checkEvent(e);
     }
-    
+
     public void mouseReleased(MouseEvent e) {
         checkEvent(e);
     }
-    
+
     private void checkEvent(MouseEvent e) {
         if (e.isPopupTrigger()) {
-            onAboutToShow(e);
-            popupMenu.show(e.getComponent(), e.getX(), e.getY());
-            popupMenu.setVisible(true);
+            if (onAboutToShow(e)) {
+                showPopupMenu(e);
+            }
         }
     }
-    
-    protected void onAboutToShow(MouseEvent e) {
-        
-    }
-
 }
