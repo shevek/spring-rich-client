e7cd5df96c72f63047cc853938530a773535d3da
Oliver Hutchison <oliverh@affirmsoftware.com.au>
2004-09-24 07:48:01 +0000

    added an interceptor that adds a nice popup and undo/redo to all text components.
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@275 817809c7-9d0e-0410-b92d-a7ac8b8adc13

---
+++ sandbox/src/org/springframework/richclient/text/TextComponentPopup.java
@@ -0,0 +1,326 @@
+/*
+ * Copyright 2002-2004 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.springframework.richclient.text;
+
+import java.awt.Toolkit;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JPasswordField;
+import javax.swing.JPopupMenu;
+import javax.swing.event.CaretEvent;
+import javax.swing.event.CaretListener;
+import javax.swing.event.UndoableEditEvent;
+import javax.swing.event.UndoableEditListener;
+import javax.swing.text.JTextComponent;
+import javax.swing.text.Keymap;
+import javax.swing.undo.UndoManager;
+
+import org.springframework.binding.value.ValueChangeListener;
+import org.springframework.binding.value.ValueModel;
+import org.springframework.richclient.application.Application;
+import org.springframework.richclient.application.ApplicationWindow;
+import org.springframework.richclient.command.ActionCommand;
+import org.springframework.richclient.command.CommandGroup;
+import org.springframework.richclient.command.CommandManager;
+import org.springframework.richclient.command.support.AbstractCommandExecutor;
+import org.springframework.richclient.command.support.DefaultCommandManager;
+import org.springframework.richclient.command.support.GlobalCommandIds;
+
+/**
+ * Helper class that decorates a <code>JTextComponent</code> with a standard
+ * popup menu. Support for undo/redo is also provided.
+ * 
+ * @author oliverh
+ */
+public class TextComponentPopup extends MouseAdapter implements FocusListener,
+        CaretListener, UndoableEditListener {
+
+    public static void attachPopup(JTextComponent textComponent,
+            ValueModel resetUndoHistoryTrigger) {
+        new TextComponentPopup(textComponent, resetUndoHistoryTrigger);
+    }
+
+    public static void attachPopup(JTextComponent textComponent) {
+        new TextComponentPopup(textComponent, null);
+    }
+
+    private JTextComponent textComponent;
+
+    private UndoManager undoManager = new UndoManager();
+
+    private ValueModel resetUndoHistoryTrigger;
+
+    private static CommandManager localCommandManager;
+
+    private final UndoCommandDelegate undo = new UndoCommandDelegate();
+
+    private final RedoCommandDelegate redo = new RedoCommandDelegate();
+
+    private final CutCommandDelegate cut = new CutCommandDelegate();
+
+    private final CopyCommandDelegate copy = new CopyCommandDelegate();
+
+    private final PasteCommandDelegate paste = new PasteCommandDelegate();
+
+    private final SelectAllCommandDelegate selectAll = new SelectAllCommandDelegate();
+
+    protected TextComponentPopup(JTextComponent textComponent,
+            ValueModel resetUndoHistoryTrigger) {
+        this.textComponent = textComponent;
+        this.resetUndoHistoryTrigger = resetUndoHistoryTrigger;
+        registerListeners();
+        registerAccelerators();
+    }
+
+    private void registerListeners() {
+        textComponent.addMouseListener(this);
+        textComponent.addFocusListener(this);
+        textComponent.addCaretListener(this);
+        textComponent.getDocument().addUndoableEditListener(this);
+        if (resetUndoHistoryTrigger != null) {
+            resetUndoHistoryTrigger
+                    .addValueChangeListener(new ValueChangeListener() {
+                        public void valueChanged() {
+                            if (Boolean.TRUE.equals(resetUndoHistoryTrigger
+                                    .getValue())) {
+                                undoManager.discardAllEdits();
+                                updateUndoRedoState();
+                            }
+                        }
+                    });
+        }
+    }
+
+    protected CommandManager getCommandManager() {
+        ApplicationWindow appWindow = Application.instance().getActiveWindow();
+        if (appWindow == null) {
+            if (localCommandManager == null) {
+                DefaultCommandManager commandManager = new DefaultCommandManager();
+                commandManager.setGlobalCommandIds(new String[] {
+                        GlobalCommandIds.UNDO, GlobalCommandIds.REDO,
+                        GlobalCommandIds.COPY, GlobalCommandIds.CUT,
+                        GlobalCommandIds.PASTE, GlobalCommandIds.SELECT_ALL });
+                localCommandManager = commandManager;
+            }
+            return localCommandManager;
+        }
+        else {
+            return appWindow.getCommandManager();
+        }
+    }
+
+    public void registerAccelerators() {
+        CommandManager commandManager = getCommandManager();        
+        Keymap keymap = textComponent.getKeymap();
+        ActionCommand command = commandManager.getActionCommand(
+                GlobalCommandIds.UNDO);
+        keymap.addActionForKeyStroke(command.getAccelerator(), command.getSwingActionAdapter());
+        command = commandManager.getActionCommand(
+                GlobalCommandIds.REDO);
+        keymap.addActionForKeyStroke(command.getAccelerator(), command.getSwingActionAdapter());
+        command = commandManager.getActionCommand(
+                GlobalCommandIds.COPY);
+        keymap.addActionForKeyStroke(command.getAccelerator(), command.getSwingActionAdapter());
+        command = commandManager.getActionCommand(
+                GlobalCommandIds.CUT);
+        keymap.addActionForKeyStroke(command.getAccelerator(), command.getSwingActionAdapter());
+        command = commandManager.getActionCommand(
+                GlobalCommandIds.PASTE);
+        keymap.addActionForKeyStroke(command.getAccelerator(), command.getSwingActionAdapter());
+        command = commandManager.getActionCommand(
+                GlobalCommandIds.SELECT_ALL);
+        keymap.addActionForKeyStroke(command.getAccelerator(), command.getSwingActionAdapter());
+    }
+
+    public void mousePressed(MouseEvent evt) {
+        if (evt.getModifiers() == MouseEvent.BUTTON3_MASK) {
+            createPopup().show(evt.getComponent(), evt.getX(), evt.getY());
+        }
+    }
+
+    public void caretUpdate(CaretEvent e) {
+        updateState();
+    }
+
+    public void focusGained(FocusEvent e) {
+        updateState();
+        registerDelgates();
+    }
+
+    public void focusLost(FocusEvent e) {
+        if (!e.isTemporary()) {
+            deRegisterDelgates();
+        }
+    }
+
+    public void undoableEditHappened(UndoableEditEvent e) {
+        undoManager.addEdit(e.getEdit());
+        updateUndoRedoState();
+    }
+
+    private JPopupMenu createPopup() {
+        if (textComponent instanceof JPasswordField) {
+            return getPasswordCommandGroup().createPopupMenu();
+        }
+        else {
+            if (isEditable()) {
+                return getEditableCommandGroup().createPopupMenu();
+            }
+            else {
+                return getReadOnlyCommandGroup().createPopupMenu();
+            }
+        }
+    }
+
+    private void updateState() {
+        boolean hasSelection = textComponent.getSelectionStart() != textComponent
+                .getSelectionEnd();
+        copy.setEnabled(hasSelection);
+        selectAll.setEnabled(textComponent.getDocument().getLength() > 0);
+        boolean isEditable = isEditable();
+        cut.setEnabled(hasSelection && isEditable);
+        boolean canPaste = isEditable
+                && textComponent.getTransferHandler().canImport(
+                        textComponent,
+                        Toolkit.getDefaultToolkit().getSystemClipboard()
+                                .getContents(textComponent)
+                                .getTransferDataFlavors());
+        paste.setEnabled(canPaste);
+        updateUndoRedoState();
+    }
+
+    private void updateUndoRedoState() {
+        undo.setEnabled(undoManager.canUndo());
+        redo.setEnabled(undoManager.canRedo());
+    }
+
+    private boolean isEditable() {
+        return !(textComponent instanceof JPasswordField)
+                && textComponent.isEnabled() && textComponent.isEditable();
+    }
+
+    protected CommandGroup getEditableCommandGroup() {
+        CommandGroup editGroup = getCommandManager().getCommandGroup(
+                "textEditMenu");
+        if (editGroup == null) {
+            editGroup = getCommandManager().createCommandGroup(
+                    "textEditMenu",
+                    new Object[] { GlobalCommandIds.UNDO,
+                            GlobalCommandIds.REDO, "separator",
+                            GlobalCommandIds.CUT, GlobalCommandIds.COPY,
+                            GlobalCommandIds.PASTE, "separator",
+                            GlobalCommandIds.SELECT_ALL });
+        }
+        return editGroup;
+    }
+
+    protected CommandGroup getPasswordCommandGroup() {
+        CommandGroup passwordGroup = getCommandManager().getCommandGroup(
+                "passwordTextEditMenu");
+        if (passwordGroup == null) {
+            passwordGroup = getCommandManager()
+                    .createCommandGroup(
+                            "passwordTextEditMenu",
+                            new Object[] { GlobalCommandIds.UNDO,
+                                    GlobalCommandIds.REDO });
+        }
+        return passwordGroup;
+    }
+
+    protected CommandGroup getReadOnlyCommandGroup() {
+        CommandGroup readOnlyGroup = getCommandManager().getCommandGroup(
+                "readOnlyTextEditMenu");
+        if (readOnlyGroup == null) {
+            readOnlyGroup = getCommandManager().createCommandGroup(
+                    "readOnlyTextEditMenu",
+                    new Object[] { GlobalCommandIds.COPY, "separator",
+                            GlobalCommandIds.SELECT_ALL });
+        }
+        return readOnlyGroup;
+    }
+
+    private void registerDelgates() {
+        CommandManager commandManager = getCommandManager();
+
+        commandManager.setTargetableActionCommandExecutor(
+                GlobalCommandIds.UNDO, undo);
+        commandManager.setTargetableActionCommandExecutor(
+                GlobalCommandIds.REDO, redo);
+        commandManager.setTargetableActionCommandExecutor(GlobalCommandIds.CUT,
+                cut);
+        commandManager.setTargetableActionCommandExecutor(
+                GlobalCommandIds.COPY, copy);
+        commandManager.setTargetableActionCommandExecutor(
+                GlobalCommandIds.PASTE, paste);
+        commandManager.setTargetableActionCommandExecutor(
+                GlobalCommandIds.SELECT_ALL, selectAll);
+    }
+
+    private void deRegisterDelgates() {
+        CommandManager commandManager = getCommandManager();
+        commandManager.setTargetableActionCommandExecutor(
+                GlobalCommandIds.UNDO, null);
+        commandManager.setTargetableActionCommandExecutor(
+                GlobalCommandIds.REDO, null);
+        commandManager.setTargetableActionCommandExecutor(GlobalCommandIds.CUT,
+                null);
+        commandManager.setTargetableActionCommandExecutor(
+                GlobalCommandIds.COPY, null);
+        commandManager.setTargetableActionCommandExecutor(
+                GlobalCommandIds.PASTE, null);
+        commandManager.setTargetableActionCommandExecutor(
+                GlobalCommandIds.SELECT_ALL, null);
+    }
+
+    private class UndoCommandDelegate extends AbstractCommandExecutor {
+        public void execute() {
+            undoManager.undo();
+        }
+    }
+
+    private class RedoCommandDelegate extends AbstractCommandExecutor {
+        public void execute() {
+            undoManager.redo();
+        }
+    }
+
+    private class CutCommandDelegate extends AbstractCommandExecutor {
+        public void execute() {
+            textComponent.cut();
+        }
+    }
+
+    private class CopyCommandDelegate extends AbstractCommandExecutor {
+        public void execute() {
+            textComponent.copy();
+        }
+    }
+
+    private class PasteCommandDelegate extends AbstractCommandExecutor {
+        public void execute() {
+            textComponent.paste();
+        }
+    }
+
+    private class SelectAllCommandDelegate extends AbstractCommandExecutor {
+        public void execute() {
+            textComponent.selectAll();
+        }
+    }
+}
---
+++ sandbox/src/org/springframework/richclient/text/TextComponentPopupInterceptorFactory.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2002-2004 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.springframework.richclient.text;
+
+import javax.swing.JComponent;
+import javax.swing.text.JTextComponent;
+
+import org.springframework.binding.form.CommitListener;
+import org.springframework.binding.form.FormModel;
+import org.springframework.binding.value.ValueChangeListener;
+import org.springframework.binding.value.support.CommitTrigger;
+import org.springframework.richclient.form.builder.FormComponentInterceptor;
+import org.springframework.richclient.form.builder.FormComponentInterceptorFactory;
+import org.springframework.richclient.form.builder.support.AbstractFormComponentInterceptor;
+
+/**
+ * @author oliverh
+ */
+public class TextComponentPopupInterceptorFactory implements
+        FormComponentInterceptorFactory {
+
+    public TextComponentPopupInterceptorFactory() {
+    }
+
+    public FormComponentInterceptor getInterceptor(FormModel formModel) {
+        return new TextComponentPopupInterceptor(formModel);
+    }
+
+    private class TextComponentPopupInterceptor extends
+            AbstractFormComponentInterceptor {
+        private CommitTrigger resetTriger;
+        
+        protected TextComponentPopupInterceptor(FormModel formModel) {
+            super(formModel);
+        }
+
+        public JComponent processComponent(String propertyName,
+                JComponent component) {
+            JComponent innerComp = getInnerComponent(component);
+            if (innerComp instanceof JTextComponent) {
+                TextComponentPopup.attachPopup((JTextComponent)innerComp,
+                        getResetTrigger());
+            }
+            return component;
+        }
+
+        private CommitTrigger getResetTrigger() {
+            if (resetTriger == null) {
+                resetTriger = new CommitTrigger();
+                registerListeners();
+            }
+            return resetTriger;
+        }
+
+        private void registerListeners() {
+            FormModel formModel = getFormModel();
+            formModel.addCommitListener(new CommitListener() {
+                public boolean preEditCommitted(Object formObject) {
+                    return true;
+                }
+
+                public void postEditCommitted(Object formObject) {
+                    resetTriger.commit();
+                }
+            });
+            formModel.addFormObjectChangeListener(new ValueChangeListener() {
+                public void valueChanged() {
+                    resetTriger.commit();
+                }
+            });
+        }
+    }
+}
