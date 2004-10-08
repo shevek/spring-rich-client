7d3fe60a5e304d041032f243e6dddfb1ca192057
Jim Moore <springrich@mooresoft.net>
2004-10-08 19:00:21 +0000

    Extracted a couple of methods out of processComponent
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@317 817809c7-9d0e-0410-b92d-a7ac8b8adc13

--- src/org/springframework/richclient/form/builder/AbstractFormBuilder.java
+++ src/org/springframework/richclient/form/builder/AbstractFormBuilder.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2002-2004 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -70,12 +70,21 @@ public abstract class AbstractFormBuilder {
 
     protected JComponent[] processComponent(final String propertyName,
             JComponent label, JComponent component) {
-        if (getInterceptor() != null) {
-            label = getInterceptor().processLabel(propertyName, label);
-            component = getInterceptor().processComponent(propertyName,
-                    component);
-        }
+        label = processLabel(propertyName, label);
+        component = processComponent(propertyName, component);
         return new JComponent[]{label, component};
+    }
+
+    protected JComponent processComponent(final String propertyName,
+                                          final JComponent component) {
+        return (getInterceptor() == null) ? component :
+            getInterceptor().processComponent(propertyName, component);
+    }
+
+    protected JComponent processLabel(final String propertyName,
+                                      final JComponent label) {
+        return (getInterceptor() == null) ? label :
+            getInterceptor().processLabel(propertyName, label);
     }
 
     protected JComponent getDefaultComponent(String propertyName) {
