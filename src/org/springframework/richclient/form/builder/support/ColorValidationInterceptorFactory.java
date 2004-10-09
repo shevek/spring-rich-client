7d0a931cf8e6951fc59e2c1834a87fb67a9697ed
Jim Moore <springrich@mooresoft.net>
2004-10-09 00:18:25 +0000

    Added javadocs
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@324 817809c7-9d0e-0410-b92d-a7ac8b8adc13

--- src/org/springframework/richclient/form/builder/support/ColorValidationInterceptorFactory.java
+++ src/org/springframework/richclient/form/builder/support/ColorValidationInterceptorFactory.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2002-2004 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -15,9 +15,9 @@
  */
 package org.springframework.richclient.form.builder.support;
 
-import java.awt.Color;
+import java.awt.*;
 
-import javax.swing.JComponent;
+import javax.swing.*;
 import javax.swing.text.JTextComponent;
 
 import org.springframework.binding.form.FormModel;
@@ -27,11 +27,16 @@ import org.springframework.richclient.form.builder.FormComponentInterceptorFacto
 import org.springframework.util.Assert;
 
 /**
+ * Adds an "overlay" to a component that is triggered by a validation event
+ * for JTextComponents.  When an error is triggered, the background color
+ * of the component is changed to the color set in
+ * {@link #setErrorColor(Color)}.  (The default color is a very light red.)
+ *
  * @author oliverh
  */
 public class ColorValidationInterceptorFactory implements
-        FormComponentInterceptorFactory {
-    
+    FormComponentInterceptorFactory {
+
     private static final Color DEFAULT_ERROR_COLOR = new Color(255, 240, 240);
 
     private Color errorColor = DEFAULT_ERROR_COLOR;
@@ -43,20 +48,20 @@ public class ColorValidationInterceptorFactory implements
         Assert.notNull(errorColor);
         this.errorColor = errorColor;
     }
-    
+
     public FormComponentInterceptor getInterceptor(FormModel formModel) {
         return new ColorValidationInterceptor(formModel);
     }
 
     private class ColorValidationInterceptor extends
-            ValidationInterceptor {
-        
-        public ColorValidationInterceptor(FormModel formModel) {            
+        ValidationInterceptor {
+
+        public ColorValidationInterceptor(FormModel formModel) {
             super(formModel);
         }
 
         public JComponent processComponent(String propertyName,
-                JComponent component) {
+                                           JComponent component) {
             JComponent innerComp = getInnerComponent(component);
             if (innerComp instanceof JTextComponent) {
                 ColorChanger colorChanger = new ColorChanger(component);
