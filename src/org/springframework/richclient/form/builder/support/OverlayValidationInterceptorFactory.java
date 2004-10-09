8b172c4ab5d62648bff8a222a61e682c4901b335
Jim Moore <springrich@mooresoft.net>
2004-10-09 00:10:28 +0000

    Added javadocs
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@323 817809c7-9d0e-0410-b92d-a7ac8b8adc13

--- src/org/springframework/richclient/form/builder/support/OverlayValidationInterceptorFactory.java
+++ src/org/springframework/richclient/form/builder/support/OverlayValidationInterceptorFactory.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2002-2004 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -15,9 +15,7 @@
  */
 package org.springframework.richclient.form.builder.support;
 
-import javax.swing.JComponent;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
+import javax.swing.*;
 
 import org.springframework.binding.form.FormModel;
 import org.springframework.richclient.application.Application;
@@ -31,36 +29,44 @@ import org.springframework.richclient.util.OverlayHelper;
 import org.springframework.rules.reporting.Severity;
 
 /**
+ * Adds an "overlay" to a component that is triggered by a validation event.
+ * The overlaid image is retrieved by the image key
+ * "severity.{severityShortCode}.overlay", where {severityShortCode} is
+ * the number returned by {@link Severity#getShortCode()}.
+ * The image is placed at the bottom-left corner of the component,
+ * and the image's tooltip is set to the validation message.
+ *
  * @author oliverh
+ * @see OverlayHelper#attachOverlay
  */
 public class OverlayValidationInterceptorFactory implements
-FormComponentInterceptorFactory {
+    FormComponentInterceptorFactory {
 
     private int textCompHeight;
 
     public OverlayValidationInterceptorFactory() {
         textCompHeight = new JTextField().getPreferredSize().height;
     }
-    
+
     public FormComponentInterceptor getInterceptor(FormModel formModel) {
         return new OverlayValidationInterceptor(formModel);
     }
 
     public class OverlayValidationInterceptor extends ValidationInterceptor {
-        
+
         public OverlayValidationInterceptor(FormModel formModel) {
             super(formModel);
         }
 
         public JComponent processComponent(String propertyName,
-                JComponent component) {
+                                           JComponent component) {
             ErrorReportingOverlay overlay = new ErrorReportingOverlay();
 
             int yOffset = component.getPreferredSize().height;
 
             OverlayHelper.attachOverlay(overlay, component,
-                    OverlayHelper.NORTH_WEST, 0, Math.min(yOffset,
-                            textCompHeight));
+                OverlayHelper.NORTH_WEST, 0, Math.min(yOffset,
+                    textCompHeight));
 
             registerErrorGuarded(propertyName, overlay);
             registerErrorMessageReceiver(propertyName, overlay);
@@ -69,7 +75,7 @@ FormComponentInterceptorFactory {
     }
 
     private class ErrorReportingOverlay extends JLabel implements
-            MessageReceiver, Guarded {
+        MessageReceiver, Guarded {
 
         private MessageBuffer messageBuffer = new MessageBuffer(this);
 
@@ -92,7 +98,7 @@ FormComponentInterceptorFactory {
             messageBuffer.setMessage(newMessage, severity);
             setToolTipText(messageBuffer.getMessage());
             setIcon(Application.services().getIcon(
-                    "severity." + severity.getShortCode() + ".overlay"));
+                "severity." + severity.getShortCode() + ".overlay"));
         }
 
         public void setErrorMessage(String errorMessage) {
@@ -107,4 +113,5 @@ FormComponentInterceptorFactory {
             messageBuffer.removeMessageListener(messageListener);
         }
     }
+
 }
