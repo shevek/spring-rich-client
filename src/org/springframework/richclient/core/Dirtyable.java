7ff209db207ad79fdb84dd5ea8edebcca5d244cb
Keith Donald <keith.donald@springsource.com>
2004-10-22 14:57:05 +0000

    saveable/dirtyable interfaces for editor components
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@424 817809c7-9d0e-0410-b92d-a7ac8b8adc13

---
+++ src/org/springframework/richclient/core/Dirtyable.java
@@ -0,0 +1,13 @@
+/*
+ * $Header: /usr/local/cvs/module/src/java/File.java,v 1.7 2004/01/16 22:23:11
+ * keith Exp $ $Revision$ $Date$
+ * 
+ * Copyright Computer Science Innovations (CSI), 2004. All rights reserved.
+ */
+package org.springframework.richclient.core;
+
+public interface Dirtyable {
+    public static final String DIRTY_PROPERTY = "dirty";
+
+    public boolean isDirty();
+}
