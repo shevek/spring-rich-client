819f1136bfbfb334e111f689d1839fef865ad8f3
Jim Moore <springrich@mooresoft.net>
2004-11-19 23:29:23 +0000

    Added lifecycle support to PageComponent
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@542 817809c7-9d0e-0410-b92d-a7ac8b8adc13

--- src/org/springframework/richclient/application/support/AbstractView.java
+++ src/org/springframework/richclient/application/support/AbstractView.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2002-2004 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -61,6 +61,18 @@ public abstract class AbstractView extends AbstractControlFactory implements Vie
         return context;
     }
 
+    public void componentOpened() {
+    }
+
+    public void componentClosed() {
+    }
+
+    public void componentFocusGained() {
+    }
+
+    public void componentFocusLost() {
+    }
+
     public String getCaption() {
         return getDescriptor().getCaption();
     }
@@ -99,7 +111,7 @@ public abstract class AbstractView extends AbstractControlFactory implements Vie
      * Template method called once when this view is initialized; allows
      * subclasses to register local executors for shared commands with the view
      * context.
-     * 
+     *
      * @param context the view context
      */
     protected void registerLocalCommandExecutors(PageComponentContext context) {
