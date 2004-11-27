377bb7625a7b19daf30be63eecfd05f7e680ecb2
Jim Moore <springrich@mooresoft.net>
2004-11-27 00:26:14 +0000

    FormModels should use RulesSource from Application if not explicitly set (RCP-42)
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@546 817809c7-9d0e-0410-b92d-a7ac8b8adc13

--- src/org/springframework/binding/form/support/AbstractFormModel.java
+++ src/org/springframework/binding/form/support/AbstractFormModel.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2002-2004 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -33,6 +33,7 @@ import org.springframework.binding.value.support.AbstractPropertyChangePublisher
 import org.springframework.binding.value.support.ValueModelWrapper;
 import org.springframework.rules.RulesSource;
 import org.springframework.util.Assert;
+import org.springframework.richclient.application.Application;
 
 /**
  * @author Oliver Hutchison
@@ -95,6 +96,9 @@ public abstract class AbstractFormModel extends AbstractPropertyChangePublisher 
     }
 
     public RulesSource getRulesSource() {
+        if (rulesSource == null) {
+            rulesSource = Application.services().getRulesSource();
+        }
         return rulesSource;
     }
 
