346e58e16e9dff32ed965f158a3baf1120bcd9fa
Marc Portier <mpo@outerthought.org>
2005-08-25 14:38:55 +0000

    Added tests for checking button-enabling of commands with multiple faces.
    [Jira: http://opensource2.atlassian.com/projects/spring/browse/RCP-73]
    
    
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@732 817809c7-9d0e-0410-b92d-a7ac8b8adc13

---
+++ test/org/springframework/richclient/command/config/MultiFacedEnablingTests.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 1999-2004 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.richclient.command.config;
+
+import javax.swing.AbstractButton;
+
+import junit.framework.TestCase;
+
+import org.springframework.richclient.command.ActionCommand;
+
+/**
+ * MultiFacedEnablingTests was built to check up on the issue reported as <a
+ * href="http://opensource.atlassian.com/projects/spring/browse/RCP-73">RCP-73 </a>.
+ */
+public class MultiFacedEnablingTests extends TestCase
+{
+    private static final String ALTERNATE_ID = "otherId";
+    private static final String MAIN_ID = "someid";
+
+    /**
+     * Big idea of the test:
+     * <ol>
+     * <li>create silly command</li>
+     * <li>register additional command face to it, and create a button that uses that.</li>
+     * <li>disable/enable the command --> check if all buttons follow up on the changes</li>
+     * </ol>
+     */
+    public void testMultifacedCommandDisabling()
+    {
+        ActionCommand command = new ActionCommand(MAIN_ID)
+        {
+
+            protected void doExecuteCommand()
+            {
+                // does nothing during this test anyway
+            }
+        };
+
+        AbstractButton standardButton = command.createButton();
+
+        // test this dude's enabling
+        command.setEnabled(false);
+        assertFalse("standard face button didn't follow up on the command's disabling", standardButton
+                .isEnabled());
+        command.setEnabled(true);
+        assertTrue("standard face button didn't follow up on the command's enabling", standardButton
+                .isEnabled());
+
+        // register an alternative face to this command
+        CommandFaceDescriptor face = new CommandFaceDescriptor();
+        command.setFaceDescriptor(ALTERNATE_ID, face);
+
+        // and get us a button with that face
+        AbstractButton otherFacedButton = command.createButton(ALTERNATE_ID);
+
+        // test this newly faced dude
+        command.setEnabled(false);
+        assertFalse("alternative face button didn't follow up on the command's disabling", otherFacedButton
+                .isEnabled());
+        command.setEnabled(true);
+        assertTrue("alternative face button didn't follow up on the command's enabling", otherFacedButton
+                .isEnabled());
+
+    }
+}
