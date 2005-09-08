8bdb9c56466cd44da67acb259bb703f51d30d707
Larry Streepy <lstreepy@nyc.rr.com>
2005-09-08 13:20:45 +0000

    Fix RCP-176: 1. Appropriate pieces are not protected so they can be overriden. 2. Getters have been provided for private members 3. New support has been added to allow usage of an AdvancedTableFormat in addition to the default WritableTableFormat. 4. Using the AdvancedTableFormat you can easily specify custom comparators and classes for each column. 5. javadoc was improved (somewhat)
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@755 817809c7-9d0e-0410-b92d-a7ac8b8adc13

--- sandbox/src/org/springframework/richclient/table/support/GlazedTableModel.java
+++ sandbox/src/org/springframework/richclient/table/support/GlazedTableModel.java
@@ -15,6 +15,9 @@
  */
 package org.springframework.richclient.table.support;
 
+import java.util.Comparator;
+import java.util.HashMap;
+
 import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.context.MessageSource;
@@ -26,12 +29,21 @@ import org.springframework.util.Assert;
 
 import ca.odell.glazedlists.BasicEventList;
 import ca.odell.glazedlists.EventList;
+import ca.odell.glazedlists.GlazedLists;
+import ca.odell.glazedlists.gui.AdvancedTableFormat;
 import ca.odell.glazedlists.gui.TableFormat;
 import ca.odell.glazedlists.gui.WritableTableFormat;
 import ca.odell.glazedlists.swing.EventTableModel;
 
 /**
  * <code>TableModel</code> that accepts a <code>EventList</code>.
+ * <p>
+ * By default, a {@link WritableTableFormat} will be generated for this model.
+ * If you want to change this, you can override the {@link #createTableFormat()}
+ * method to provide your own format.  In addition, an implementation of an
+ * {@link AdvancedTableFormat} is provided for use.  It allows for the specification
+ * of an object prototype (for determining column classes) and the ability to specify
+ * comparators per column for sorting support.
  * 
  * @author Peter De Bruycker
  */
@@ -60,6 +72,7 @@ public class GlazedTableModel extends EventTableModel {
         Assert.notEmpty(columnPropertyNames, "ColumnPropertyNames parameter cannot be null.");
         this.columnPropertyNames = columnPropertyNames;
         setMessageSource(messageSource);
+        columnLabels = createColumnNames(columnPropertyNames);
         setTableFormat(createTableFormat());
     }
 
@@ -90,6 +103,14 @@ public class GlazedTableModel extends EventTableModel {
     protected Object getColumnValue(Object row, int column) {
         beanWrapper.setWrappedInstance(row);
         return beanWrapper.getPropertyValue(columnPropertyNames[column]);
+    }
+
+    protected String[] getColumnLabels() {
+        return columnLabels;
+    }
+
+    protected String[] getColumnPropertyNames() {
+        return columnPropertyNames;
     }
 
     /**
@@ -140,29 +161,133 @@ public class GlazedTableModel extends EventTableModel {
         return columnNames;
     }
 
-    private TableFormat createTableFormat() {
-        columnLabels = createColumnNames(columnPropertyNames);
-        return new WritableTableFormat() {
+    protected TableFormat createTableFormat() {
+        return new DefaultTableFormat();
+    }
 
-            public int getColumnCount() {
-                return columnLabels.length;
+    /**
+     * This inner class is the default TableFormat constructed.  In order to
+     * extend this class you will also need to override
+     * {@link GlazedTableModel#createTableFormat()} to instantiate an instance
+     * of your derived table format.
+     */
+    protected class DefaultTableFormat implements WritableTableFormat {
+
+        public int getColumnCount() {
+            return columnLabels.length;
+        }
+
+        public String getColumnName(int column) {
+            return columnLabels[column];
+        }
+
+        public Object getColumnValue(Object row, int column) {
+            return GlazedTableModel.this.getColumnValue(row, column);
+        }
+
+        public boolean isEditable(Object row, int column) {
+            return GlazedTableModel.this.isEditable(row, column);
+        }
+
+        public Object setColumnValue(Object row, Object value, int column) {
+            return GlazedTableModel.this.setColumnValue(row, value, column);
+        }
+    }
+    
+    /**
+     * This inner class can be used by derived implementations to use an
+     * AdvancedTableFormat instead of the default WritableTableFormat created by
+     * {@link GlazedTableModel#createTableFormat()}.
+     * <p>
+     * If a prototype value is not provided (see {@link #setPrototypeValue(Object)}, then
+     * the default implementation of getColumnClass will return Object.class, which is not
+     * very usable. In that case, you should probably override
+     * {@link #getColumnClass(int)}.
+     * <p>
+     * You can specify individual comparators for columns using
+     * {@link #setComparator(int, Comparator)}. For any column that doesn't have a
+     * comparator installed, a default comparable comparator will be handed out by
+     * {@link #getColumnComparator(int)}.
+     */
+    protected class DefaultAdvancedTableFormat implements AdvancedTableFormat {
+
+        public DefaultAdvancedTableFormat() {
+        }
+
+        public int getColumnCount() {
+            return getColumnLabels().length;
+        }
+
+        public String getColumnName(int column) {
+            return getColumnLabels()[column];
+        }
+
+        public Object getColumnValue(Object row, int column) {
+            return GlazedTableModel.this.getColumnValue( row, column );
+        }
+
+        /**
+         * Returns the class for all the cell values in the column. This is used by the
+         * table to set up a default renderer and editor for the column. If a prototype
+         * object has been specified, then the class will be obtained using introspection
+         * using the property name associated with the specified column. If no prorotype
+         * has been specified, then <code>Object.class</code> is returned.
+         * 
+         * @param column The index of the column being edited.
+         */
+        public Class getColumnClass(int column) {
+            Class cls = Object.class;
+            Integer columnKey = new Integer(column);
+            if( _prototype != null ) {
+                cls = (Class)_columnClasses.get(columnKey);
+                if( cls == null ) {
+                    cls = _beanWrapper.getPropertyType( getColumnPropertyNames()[column] );
+                    _columnClasses.put(columnKey, cls);
+                }
             }
 
-            public String getColumnName(int column) {
-                return columnLabels[column];
-            }
+            return cls;
+        }
 
-            public Object getColumnValue(Object row, int column) {
-                return GlazedTableModel.this.getColumnValue(row, column);
-            }
+        /**
+         * Get the comparator to use on values in the given column. If a comparator for
+         * this column has been installed by calling
+         * {@link #setComparator(int, Comparator)}, then it is returned. If not, then a
+         * default comparator (assuming the objects implement Comparable) is returned.
+         * 
+         * @param column the column
+         * @return the {@link Comparator} to use or <code>null</code> for an unsortable
+         *         column.
+         */
+        public Comparator getColumnComparator(int column) {
+            Comparator comparator = (Comparator)_comparators.get(new Integer(column));
+            return comparator != null ? comparator : GlazedLists.comparableComparator();
+        }
 
-            public boolean isEditable(Object row, int column) {
-                return GlazedTableModel.this.isEditable(row, column);
-            }
+        /**
+         * Set the comparator to use for a given column.
+         * 
+         * @param column The column for which the compartor is to be used
+         * @param comparator The comparator to install
+         */
+        public void setComparator(int column, Comparator comparator) {
+            _comparators.put(new Integer(column), comparator);
+        }
 
-            public Object setColumnValue(Object row, Object value, int column) {
-                return GlazedTableModel.this.setColumnValue(row, value, column);
-            }
-        };
+        /**
+         * Set the prototype value from which to determine column classes. If a prototype
+         * value is not provided, then the default implementation of getColumnClass will
+         * return Object.class, which is not very usable. If you don't provide a
+         * prototype, you should probably override {@link #getColumnClass(int)}.
+         */
+        public void setPrototypeValue(Object prototype) {
+            _prototype = prototype;
+            _beanWrapper = new BeanWrapperImpl( _prototype );
+        }
+
+        private HashMap _comparators = new HashMap();
+        private HashMap _columnClasses = new HashMap();
+        private Object _prototype;
+        private BeanWrapper _beanWrapper;
     }
 }
