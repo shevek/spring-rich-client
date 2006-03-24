511ce3de300cc8030f2387a3c6db9c0f9b11d037
Oliver Hutchison <oliverh@affirmsoftware.com.au>
2006-03-24 00:05:20 +0000

    Doing some tests to make sure that rename and move refactorings are properly converted into SVN copy/delete and rename functions when using Eclipse with Subversive SVN client.
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@968 817809c7-9d0e-0410-b92d-a7ac8b8adc13

--- support/src/main/java/SVNTest.java
+++ support/src/main/java/SVNTest.java
@@ -1,9 +1,9 @@
-package org;
+
 
 /**
  * Dummy class for testing that move and rename refactoring in Eclipse with Subversive 
  * keep version history. 
  */
-public class SVNTestRenamed {
+public class SVNTest {
 
 }
