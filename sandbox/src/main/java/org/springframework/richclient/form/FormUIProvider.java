f0436ad7ecb3f2fa9bc19fc4f05d934d573e5b93
Andy DePue <andy@marathon-man.com>
2006-03-30 18:53:35 +0000

    Added initial interface for pre-generated (designed) form UIs to sandbox as per RCP-306
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@993 817809c7-9d0e-0410-b92d-a7ac8b8adc13

---
+++ sandbox/src/main/java/org/springframework/richclient/form/FormUIProvider.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2002-2006 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ */
+
+package org.springframework.richclient.form;
+
+import javax.swing.JComponent;
+
+import org.springframework.richclient.factory.ControlFactory;
+import org.springframework.richclient.form.binding.BindingFactory;
+
+/**
+ * Allows pre-generated form UIs to easily integrate with Spring Rich's form
+ * and binding framework.  Typically, these pre-generated form UIs are from
+ * 3rd party form designers (Matisse, JFormDesigner, etc).
+ *
+ * @author Andy DePue
+ * @author Peter De Bruycker
+ * @author Christophe GADAIX
+ */
+public interface FormUIProvider extends ControlFactory {
+
+  /**
+   * Produces the pre-generated form as a single Swing component.
+   */
+  JComponent getControl();
+
+  /**
+   * Binds the fields and other components in this pre-generated form to a
+   * Spring {@link Form form} by using the specified {@link BindingFactory}.
+   * 
+   * @param factory the <code>BindingFactory</code> this form provider should
+   *        use to bind the provided form.
+   */
+  void bind(BindingFactory factory);
+
+  /**
+   * Provides access to individual components of this pre-generated form.
+   * Components are referenced by id.  The "id" of a component can be any
+   * arbitrary String agreed upon between the designer and the developer, but
+   * typically these IDs will be the same as the property names of the
+   * object backing the form.
+   *
+   * @param componentId
+   *
+   * @return component with the specified id, or <code>null</code> if no
+   *         component exists in this pre-generated form with the given id.
+   */
+  JComponent getComponent(String componentId);
+}
---
+++ sandbox/src/main/java/org/springframework/richclient/form/GeneratedForm.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2002-2006 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ */
+
+package org.springframework.richclient.form;
+
+import javax.swing.JComponent;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.util.Assert;
+
+/**
+ * Convenience class for producing a Spring Rich {@link Form} based on a
+ * pre-generated form UI (typically, a form that has been created in a form
+ * designer such as Matisse or JFormDesigner).  This implementation handles
+ * the most common case where a developer designs a form and wants to display
+ * it without any custom functionality beyond what Spring Rich already
+ * provides.
+ *
+ * @author Andy DePue
+ * @author Peter De Bruycker
+ */
+public class GeneratedForm extends AbstractForm implements InitializingBean {
+  private FormUIProvider formUIProvider;
+
+  //
+  // METHODS FROM CLASS AbstractForm
+  //
+
+  protected JComponent createFormControl() {
+    this.formUIProvider.bind(getBindingFactory());
+    return this.formUIProvider.getControl();
+  }
+
+
+
+  //
+  // METHODS FROM INTERFACE InitializingBean
+  //
+
+  public void afterPropertiesSet() throws Exception {
+    Assert.notNull(this.formUIProvider, "formUIProvider must be set");
+  }
+
+
+
+
+  //
+  // SIMPLE PROPERTY ACCESSORS
+  //
+
+  public FormUIProvider getFormUIProvider() {
+    return formUIProvider;
+  }
+
+  public void setFormUIProvider(final FormUIProvider formUIProvider) {
+    this.formUIProvider = formUIProvider;
+  }
+}
