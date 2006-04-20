251d87e96e1fd1bb71ae6e29af4825efcb3ad04a
Larry Streepy <lstreepy@nyc.rr.com>
2006-04-20 18:43:57 +0000

    cleanup compiler warnings
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@1074 817809c7-9d0e-0410-b92d-a7ac8b8adc13

--- support/src/main/java/org/springframework/richclient/util/BrowserLauncher.java
+++ support/src/main/java/org/springframework/richclient/util/BrowserLauncher.java
@@ -149,12 +149,6 @@ public class BrowserLauncher {
     /** The kAnyTransactionID AppleEvent code */
     private static Integer kAnyTransactionID;
 
-    /** The linkage object required for JDirect 3 on Mac OS X. */
-    private static Object linkage;
-
-    /** The framework to reference on Mac OS X */
-    private static final String JDirect_MacOSX = "/System/Library/Frameworks/Carbon.framework/Frameworks/HIToolbox.framework/HIToolbox";
-
     /** JVM constant for MRJ 2.0 */
     private static final int MRJ_2_0 = 0;
 
@@ -372,7 +366,7 @@ public class BrowserLauncher {
             try {
                 Class linker = Class.forName("com.apple.mrj.jdirect.Linker");
                 Constructor constructor = linker.getConstructor(new Class[] {Class.class});
-                linkage = constructor.newInstance(new Object[] {BrowserLauncher.class});
+                constructor.newInstance(new Object[] {BrowserLauncher.class});
             }
             catch (ClassNotFoundException cnfe) {
                 errorMessage = cnfe.getMessage();
