84f1da0f2a710fe2653703e8594980cb22015a90
Jan Hoskens <jh@schaubroeck.be>
2006-06-14 07:16:12 +0000

    NumberBinding that can be reused. See javadoc in NumberBinder for possible configurations.
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@1200 817809c7-9d0e-0410-b92d-a7ac8b8adc13

---
+++ sandbox/src/main/java/org/springframework/richclient/swing/UserInputListener.java
@@ -0,0 +1,12 @@
+package org.springframework.richclient.swing;
+
+import javax.swing.JComponent;
+
+/**
+ * Separate listener because when format is changed during focus lost/gained,
+ * ordinary DocumentListener fires events. This listener can be used to listen
+ * to userInputEvents only.
+ */
+public interface UserInputListener {
+    public void update(JComponent source);
+}
