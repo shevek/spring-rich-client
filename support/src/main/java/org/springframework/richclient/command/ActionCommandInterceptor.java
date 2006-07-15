0d6cebfb67cb56d5ba6849c7026441b664406c4d
Mathias Broekelmann <mbroekelmann@googlemail.com>
2006-07-15 06:59:08 +0000

    added javadoc comments
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@1224 817809c7-9d0e-0410-b92d-a7ac8b8adc13

--- support/src/main/java/org/springframework/richclient/command/ActionCommandInterceptor.java
+++ support/src/main/java/org/springframework/richclient/command/ActionCommandInterceptor.java
@@ -15,8 +15,30 @@
  */
 package org.springframework.richclient.command;
 
+/**
+ * Implementations of this interface can be assigned to an action command to
+ * intercept command execution.
+ * 
+ * @author Keith Donald, Mathias Broekelmann
+ */
 public interface ActionCommandInterceptor {
-    public boolean preExecution(ActionCommand command);
 
-    public void postExecution(ActionCommand command);
+	/**
+	 * Will be called before the action command is executed.
+	 * 
+	 * @param command
+	 *            the action command which gets executed
+	 * @return If false the action command and any subsequent action command
+	 *         interceptor will not get executed. If true the action command
+	 *         will be executed.
+	 */
+	public boolean preExecution(ActionCommand command);
+
+	/**
+	 * Will be called after successfull execution of an action command.
+	 * 
+	 * @param command
+	 *            the action command which was executed.
+	 */
+	public void postExecution(ActionCommand command);
 }
