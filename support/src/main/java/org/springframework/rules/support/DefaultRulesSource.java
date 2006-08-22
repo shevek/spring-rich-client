9f63971cdcc47f5d2c632b8c96577262a016923a
Mathias Broekelmann <mbroekelmann@googlemail.com>
2006-08-22 07:15:22 +0000

    support rules for abstract types
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@1330 817809c7-9d0e-0410-b92d-a7ac8b8adc13

--- support/src/main/java/org/springframework/rules/support/DefaultRulesSource.java
+++ support/src/main/java/org/springframework/rules/support/DefaultRulesSource.java
@@ -23,99 +23,103 @@ import java.util.Map;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.core.style.ToStringCreator;
+import org.springframework.richclient.util.ClassUtils;
 import org.springframework.rules.Rules;
 import org.springframework.rules.RulesSource;
 import org.springframework.rules.constraint.ConstraintsAccessor;
 import org.springframework.rules.constraint.property.PropertyConstraint;
+import org.springframework.util.Assert;
+import org.springframework.util.CachingMapDecorator;
 import org.springframework.util.StringUtils;
 
 /**
- * A default rules source implementation which is simply a in-memory registry
- * for bean validation rules backed by a map.
+ * A default rules source implementation which is simply a in-memory registry for bean validation rules backed by a map.
  * 
  * @author Keith Donald
  */
 public class DefaultRulesSource extends ConstraintsAccessor implements RulesSource {
-	protected final Log logger = LogFactory.getLog(getClass());
+    protected final Log logger = LogFactory.getLog(getClass());
 
-	private static final String DEFAULT_CONTEXT_ID = "default";
+    private static final String DEFAULT_CONTEXT_ID = "default";
 
-	private Map ruleContexts = new HashMap();
+    private Map ruleContexts = new CachingMapDecorator() {
+        protected Object create(Object key) {
+            return new HashMap();
+        }
+    };
 
-	/**
-	 * Add or update the rules for a single bean class.
-	 * 
-	 * @param rules
-	 *            The rules.
-	 */
-	public void addRules(Rules rules) {
-		if (logger.isDebugEnabled()) {
-			logger.debug("Adding rules -> " + rules);
-		}
-		addRules(DEFAULT_CONTEXT_ID, rules);
-	}
+    /**
+     * Add or update the rules for a single bean class.
+     * 
+     * @param rules
+     *            The rules.
+     */
+    public void addRules(Rules rules) {
+        if (logger.isDebugEnabled()) {
+            logger.debug("Adding rules -> " + rules);
+        }
+        addRules(DEFAULT_CONTEXT_ID, rules);
+    }
 
-	public void addRules(String contextId, Rules rules) {
-		Map context = getOrCreateRuleContext(contextId);
-		context.put(rules.getDomainObjectType(), rules);
-	}
+    public void addRules(String contextId, Rules rules) {
+        Assert.notNull(contextId);
+        Assert.notNull(rules);
+        Map context = getRuleContext(contextId);
+        context.put(rules.getDomainObjectType(), rules);
+    }
 
-	private Map getOrCreateRuleContext(String contextId) {
-		Map context = (Map)this.ruleContexts.get(contextId);
-		if (context == null) {
-			context = new HashMap();
-			this.ruleContexts.put(contextId, context);
-		}
-		return context;
-	}
+    private Map getRuleContext(String contextId) {
+        return (Map) ruleContexts.get(contextId);
+    }
 
-	/**
-	 * Set the list of rules retrievable by this source, where each item in the
-	 * list is a <code>Rules</code> object which maintains validation rules
-	 * for a bean class.
-	 * 
-	 * @param rules
-	 *            The list of rules.
-	 */
-	public void setRules(List rules) {
-		if (logger.isDebugEnabled()) {
-			logger.debug("Configuring rules in source...");
-		}
-		getOrCreateRuleContext(DEFAULT_CONTEXT_ID).clear();
-		for (Iterator i = rules.iterator(); i.hasNext();) {
-			addRules((Rules)i.next());
-		}
-	}
+    /**
+     * Set the list of rules retrievable by this source, where each item in the list is a <code>Rules</code> object
+     * which maintains validation rules for a bean class.
+     * 
+     * @param rules
+     *            The list of rules.
+     */
+    public void setRules(List rules) {
+        Assert.notNull(rules);
+        if (logger.isDebugEnabled()) {
+            logger.debug("Configuring rules in source...");
+        }
+        getRuleContext(DEFAULT_CONTEXT_ID).clear();
+        for (Iterator i = rules.iterator(); i.hasNext();) {
+            addRules((Rules) i.next());
+        }
+    }
 
-	public Rules getRules(Class bean) {
-		return getRules(bean, DEFAULT_CONTEXT_ID);
-	}
+    public Rules getRules(Class bean) {
+        return getRules(bean, DEFAULT_CONTEXT_ID);
+    }
 
-	public Rules getRules(Class bean, String contextId) {
-		if (!StringUtils.hasText(contextId)) {
-			contextId = DEFAULT_CONTEXT_ID;
-		}
-		return (Rules)getOrCreateRuleContext(contextId).get(bean);
-	}
+    public Rules getRules(Class beanType, String contextId) {
+        Assert.notNull(beanType);
+        if (!StringUtils.hasText(contextId)) {
+            contextId = DEFAULT_CONTEXT_ID;
+        }
+        return (Rules) ClassUtils.getValueFromMapForClass(beanType, getRuleContext(contextId));
+    }
 
-	public PropertyConstraint getPropertyConstraint(Class bean, String propertyName) {
-		return getPropertyConstraint(bean, propertyName, DEFAULT_CONTEXT_ID);
-	}
+    public PropertyConstraint getPropertyConstraint(Class bean, String propertyName) {
+        return getPropertyConstraint(bean, propertyName, DEFAULT_CONTEXT_ID);
+    }
 
-	public PropertyConstraint getPropertyConstraint(Class bean, String propertyName, String contextId) {
-		if (logger.isDebugEnabled()) {
-			logger.debug("Retrieving rules for bean '" + bean + "', context = " + contextId + ", property '" + propertyName
-					+ "'");
-		}
-		Rules rules = getRules(bean, contextId);
-		if (rules != null)
-			return rules.getPropertyConstraint(propertyName);
+    public PropertyConstraint getPropertyConstraint(Class bean, String propertyName, String contextId) {
+        if (logger.isDebugEnabled()) {
+            logger.debug("Retrieving rules for bean '" + bean + "', context = " + contextId + ", property '"
+                    + propertyName + "'");
+        }
+        Rules rules = getRules(bean, contextId);
+        if (rules != null)
+            return rules.getPropertyConstraint(propertyName);
 
         return null;
-	}
+    }
 
-	public String toString() {
-		return new ToStringCreator(this).append("rules", ruleContexts).toString();
-	}
+    public String toString() {
+        return new ToStringCreator(this).append("rules", ruleContexts).toString();
+    }
 
 }
