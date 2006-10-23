4761543612dea6070bf5464907277445e167522d
Peter De Bruycker <peter.de.bruycker+spring-rich@gmail.com>
2006-10-23 08:08:58 +0000

    - cleaned up interface (removed obsolete methods) - added close(PageComponent) method - added file comment
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@1528 817809c7-9d0e-0410-b92d-a7ac8b8adc13

--- support/src/main/java/org/springframework/richclient/application/ApplicationPage.java
+++ support/src/main/java/org/springframework/richclient/application/ApplicationPage.java
@@ -1,29 +1,45 @@
+/*
+ * Copyright 2002-2006 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package org.springframework.richclient.application;
 
 import org.springframework.richclient.factory.ControlFactory;
 
+/**
+ * An <code>ApplicationPage</code> is a container for <code>PageComponent</code>s.
+ */
 public interface ApplicationPage extends ControlFactory {
-    public String getId();
+    String getId();
 
-    public ApplicationWindow getWindow();
+    ApplicationWindow getWindow();
 
-    public void addPageComponentListener(PageComponentListener listener);
+    void addPageComponentListener(PageComponentListener listener);
 
-    public void removePageComponentListener(PageComponentListener listener);
+    void removePageComponentListener(PageComponentListener listener);
 
-    public PageComponent getActiveComponent();
+    PageComponent getActiveComponent();
 
-    public void showView(String viewDescriptorId);
+    void showView(String viewDescriptorId);
 
-    public void showView(ViewDescriptor viewDescriptor);
+    void showView(ViewDescriptor viewDescriptor);
 
-    public void openEditor(Object editorInput);
+    void openEditor(Object editorInput);
 
-    public boolean closeAllEditors();
+    boolean closeAllEditors();
 
-    public boolean close();
+    boolean close();
     
-    public void setApplicationWindow(ApplicationWindow window);
-    
-    public void setDescriptor(PageDescriptor descriptor);
+    void close(PageComponent pageComponent);
 }
