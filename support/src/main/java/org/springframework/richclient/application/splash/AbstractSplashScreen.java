dbbdc046eab38562a78ea92b32cab5b4cac7e081
Peter De Bruycker <peter.de.bruycker+spring-rich@gmail.com>
2006-10-24 11:13:19 +0000

    RCP-126: add drop shadow to splash screen
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@1538 817809c7-9d0e-0410-b92d-a7ac8b8adc13

--- support/src/main/java/org/springframework/richclient/application/splash/AbstractSplashScreen.java
+++ support/src/main/java/org/springframework/richclient/application/splash/AbstractSplashScreen.java
@@ -20,16 +20,13 @@ import java.awt.Image;
 import java.awt.Toolkit;
 import java.net.URL;
 
-import javax.swing.JComponent;
 import javax.swing.JFrame;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.context.MessageSource;
 import org.springframework.context.NoSuchMessageException;
-import org.springframework.richclient.progress.NullProgressMonitor;
-import org.springframework.richclient.progress.ProgressMonitor;
-import org.springframework.util.Assert;
+import org.springframework.richclient.control.ShadowBorderFrame;
 
 /**
  * An abstract helper implementation of the {@link SplashScreen} interface.
@@ -43,131 +40,158 @@ import org.springframework.util.Assert;
  * @author Peter De Bruycker
  */
 public abstract class AbstractSplashScreen implements SplashScreen {
-    
-    /**
-     * The message resource key used to look up the splash screen's frame title.
-     */
-    public static final String SPLASH_TITLE_KEY = "splash.title";
-    
-    private JFrame frame;
-    private MessageSource messageSource;
-    private String iconResourcePath;
-    
-    protected final Log logger = LogFactory.getLog(getClass());
 
-    /**
-     * Returns the location of the image to be used as the icon for the splash
-     * screen's frame. The splash screen produced by this class is undecorated, 
-     * so the icon will only be visible in the frame's taskbar icon. If the given 
-     * path starts with a '/', it is interpreted to be relative to the root of the 
-     * runtime classpath. Otherwise it is interpreted to be relative to the 
-     * subdirectory of the classpath root that corresponds to the package of this 
-     * class. 
-     *
-     * @return The location of the icon resource.
-     */
-    public String getIconResourcePath() {
-        return iconResourcePath;
-    }
+	/**
+	 * The message resource key used to look up the splash screen's frame title.
+	 */
+	public static final String SPLASH_TITLE_KEY = "splash.title";
 
-    /**
-     * Sets the location of the image to be used as the icon for the splash
-     * screen's frame. The splash screen produced by this class is undecorated, 
-     * so the icon will only be visible in the frame's taskbar icon. If the given 
-     * path starts with a '/', it is interpreted to be relative to the root of the 
-     * runtime classpath. Otherwise it is interpreted to be relative to the 
-     * subdirectory of the classpath root that corresponds to the package of this 
-     * class. 
-     *
-     * @param iconResourcePath The location of the icon resource.
-     */
-    public void setIconResourcePath(String iconResourcePath) {
-        this.iconResourcePath = iconResourcePath;
-    }
+	private JFrame frame;
 
-    /**
-     * Returns the message source used to resolve localized messages.
-     *
-     * @return The message source, or null.
-     */
-    public MessageSource getMessageSource() {
-        return messageSource;
-    }
+	private MessageSource messageSource;
 
-    /**
-     * Sets the message source used to resolve localized messages.
-     * 
-     * @param messageSource The message source.
-     */
-    public void setMessageSource(MessageSource messageSource) {
-        this.messageSource = messageSource;
-    }
+	private String iconResourcePath;
 
-    public final void dispose() {
-        
-        if (frame != null) {
-            frame.dispose();
-            frame = null;
-        }
-        
-    }
+	private boolean shadowBorder;
 
-    /**
-     * Creates and displays an undecorated, centered splash screen containing the 
-     * component provided by {@link #createContentPane()}. If this instance
-     * has been provided with a {@link MessageSource}, it will be used to retrieve 
-     * the splash screen's frame title under the key {@value #SPLASH_TITLE_KEY}. 
-     */
-    public final void splash() {
-        frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-        frame.setUndecorated(true);
+	protected final Log logger = LogFactory.getLog(getClass());
 
-        frame.setTitle(loadFrameTitle());
-        frame.setIconImage(loadFrameIcon());
+	/**
+	 * Returns the location of the image to be used as the icon for the splash
+	 * screen's frame. The splash screen produced by this class is undecorated,
+	 * so the icon will only be visible in the frame's taskbar icon. If the
+	 * given path starts with a '/', it is interpreted to be relative to the
+	 * root of the runtime classpath. Otherwise it is interpreted to be relative
+	 * to the subdirectory of the classpath root that corresponds to the package
+	 * of this class.
+	 * 
+	 * @return The location of the icon resource.
+	 */
+	public String getIconResourcePath() {
+		return iconResourcePath;
+	}
 
-        Component content = createContentPane();
-        if(content != null) {
-            frame.getContentPane().add(content);
-        }
-        frame.pack();
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-    }
+	/**
+	 * Sets the location of the image to be used as the icon for the splash
+	 * screen's frame. The splash screen produced by this class is undecorated,
+	 * so the icon will only be visible in the frame's taskbar icon. If the
+	 * given path starts with a '/', it is interpreted to be relative to the
+	 * root of the runtime classpath. Otherwise it is interpreted to be relative
+	 * to the subdirectory of the classpath root that corresponds to the package
+	 * of this class.
+	 * 
+	 * @param iconResourcePath The location of the icon resource.
+	 */
+	public void setIconResourcePath(String iconResourcePath) {
+		this.iconResourcePath = iconResourcePath;
+	}
 
-    /**
-     * Loads the message under the key {@value #SPLASH_TITLE_KEY} if 
-     * a {@link MessageSource} has been set on this instance.
-     * 
-     * @return The message resource stored under the key {@value #SPLASH_TITLE_KEY},
-     * or null if no message source has been set.
-     */
-    private String loadFrameTitle() {
-        try {
-            return messageSource == null ? null : messageSource.getMessage(SPLASH_TITLE_KEY, null, null);
-        } catch (NoSuchMessageException e) {
-            return null;
-        }
-    }
+	/**
+	 * Returns the message source used to resolve localized messages.
+	 * 
+	 * @return The message source, or null.
+	 */
+	public MessageSource getMessageSource() {
+		return messageSource;
+	}
 
-    private Image loadFrameIcon() {
-        if (iconResourcePath == null) {
-            return null;
-        }
+	/**
+	 * Sets the message source used to resolve localized messages.
+	 * 
+	 * @param messageSource The message source.
+	 */
+	public void setMessageSource(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
 
-        URL url = this.getClass().getResource(iconResourcePath);
-        if (url == null) {
-            logger.warn("Unable to locate splash screen in classpath at: " + iconResourcePath);
-            return null;
-        }
-        return Toolkit.getDefaultToolkit().createImage(url);
-    }
+	public final void dispose() {
 
-    /**
-     * Returns the component to be displayed in the splash screen's main frame. If the returned value is null the frame
-     * for the splash screen will still be created but will not have any content
-     * 
-     * @return The content pane component. Can be null.
-     */
-    protected abstract Component createContentPane();
+		if (frame != null) {
+			frame.dispose();
+			frame = null;
+		}
+
+	}
+
+	/**
+	 * Creates and displays an undecorated, centered splash screen containing
+	 * the component provided by {@link #createContentPane()}. If this instance
+	 * has been provided with a {@link MessageSource}, it will be used to
+	 * retrieve the splash screen's frame title under the key
+	 * {@value #SPLASH_TITLE_KEY}.
+	 */
+	public final void splash() {
+		frame = shadowBorder ? new ShadowBorderFrame() : new JFrame();
+		frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		frame.setUndecorated(true);
+
+		frame.setTitle(loadFrameTitle());
+		frame.setIconImage(loadFrameIcon());
+
+		Component content = createContentPane();
+		if (content != null) {
+			frame.getContentPane().add(content);
+		}
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
+
+	/**
+	 * Loads the message under the key {@value #SPLASH_TITLE_KEY} if a
+	 * {@link MessageSource} has been set on this instance.
+	 * 
+	 * @return The message resource stored under the key
+	 * {@value #SPLASH_TITLE_KEY}, or null if no message source has been set.
+	 */
+	private String loadFrameTitle() {
+		try {
+			return messageSource == null ? null : messageSource.getMessage(SPLASH_TITLE_KEY, null, null);
+		}
+		catch (NoSuchMessageException e) {
+			return null;
+		}
+	}
+
+	private Image loadFrameIcon() {
+		if (iconResourcePath == null) {
+			return null;
+		}
+
+		URL url = this.getClass().getResource(iconResourcePath);
+		if (url == null) {
+			logger.warn("Unable to locate splash screen in classpath at: " + iconResourcePath);
+			return null;
+		}
+		return Toolkit.getDefaultToolkit().createImage(url);
+	}
+
+	/**
+	 * Returns the component to be displayed in the splash screen's main frame.
+	 * If the returned value is null the frame for the splash screen will still
+	 * be created but will not have any content
+	 * 
+	 * @return The content pane component. Can be null.
+	 */
+	protected abstract Component createContentPane();
+
+	/**
+	 * Returns whether the splash screen will be rendered with a shadow border.
+	 * The shadow border will let the background show through.
+	 * 
+	 * @return whether to show a shadow border or not
+	 */
+	public boolean isShadowBorder() {
+		return shadowBorder;
+	}
+
+	/**
+	 * Sets whether the splash screen will be rendered with a shadow border. If
+	 * <code>true</code> a border will be shown.
+	 * 
+	 * @param shadowBorder Show the shadow border or not
+	 */
+	public void setShadowBorder(boolean shadowBorder) {
+		this.shadowBorder = shadowBorder;
+	}
 }
