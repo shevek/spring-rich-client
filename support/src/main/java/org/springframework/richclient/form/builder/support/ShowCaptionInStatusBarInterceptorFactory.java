5addba9b0c3ed6571c88a3bf847e0544631e4987
Peter De Bruycker <peter.de.bruycker+spring-rich@gmail.com>
2006-10-26 06:39:59 +0000

    promoted ShowCaptionInStatusBarInterceptorFactory to the support module
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@1548 817809c7-9d0e-0410-b92d-a7ac8b8adc13

---
+++ support/src/main/java/org/springframework/richclient/form/builder/support/ShowCaptionInStatusBarInterceptorFactory.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2002-2006 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.springframework.richclient.form.builder.support;
+
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+
+import javax.swing.JComponent;
+
+import org.springframework.binding.form.FormModel;
+import org.springframework.richclient.application.Application;
+import org.springframework.richclient.form.builder.FormComponentInterceptor;
+import org.springframework.richclient.form.builder.FormComponentInterceptorFactory;
+import org.springframework.richclient.form.builder.support.AbstractFormComponentInterceptor;
+
+/**
+ * If a form property has a caption defined in the messages.properties file the following
+ * happens: When component receives focus, the value is shown in the statusbar, when the
+ * component loses focus, the statusbar is emptied.
+ * 
+ * @author Peter De Bruycker
+ */
+public class ShowCaptionInStatusBarInterceptorFactory implements FormComponentInterceptorFactory {
+
+    public FormComponentInterceptor getInterceptor( FormModel formModel ) {
+        return new FocusIndicatorInterceptor( formModel );
+    }
+
+    private class FocusIndicatorInterceptor extends AbstractFormComponentInterceptor {
+
+        private FormModel formModel;
+
+        public FocusIndicatorInterceptor( FormModel formModel ) {
+            this.formModel = formModel;
+        }
+
+        String getCaption( String propertyName ) {
+            return formModel.getFieldFace( propertyName ).getCaption();
+        }
+
+        public void processComponent( final String propertyName, final JComponent component ) {
+            component.addFocusListener( new FocusListener() {
+                public void focusGained( FocusEvent e ) {
+                    if( Application.instance().getActiveWindow() != null ) {
+                        String caption = getCaption( propertyName );
+                        if( caption != null ) {
+                            Application.instance().getActiveWindow().getStatusBar().setMessage( caption );
+                        }
+                    }
+                }
+
+                public void focusLost( FocusEvent e ) {
+                    if( Application.instance().getActiveWindow() != null ) {
+                        Application.instance().getActiveWindow().getStatusBar().setMessage( "" );
+                    }
+                }
+            } );
+        }
+    }
+}
