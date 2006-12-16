71742480d3a59e801579d7be7e0ae017697f087a
Kevin Stembridge <kevin.stembridge@gmail.com>
2006-12-16 16:44:15 +0000

    Added Javadoc
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@1597 817809c7-9d0e-0410-b92d-a7ac8b8adc13

--- support/src/main/java/org/springframework/richclient/core/Guarded.java
+++ support/src/main/java/org/springframework/richclient/core/Guarded.java
@@ -16,13 +16,24 @@
 package org.springframework.richclient.core;
 
 /**
- * Interface to be implemented by controls that can be enabled or disabled based
- * on context.
+ * Interface to be implemented by objects that can be enabled or disabled. For example, these 
+ * objects will typically be user interface controls that the application will want 
+ * to disable in certain contexts. 
  * 
  * @author Keith Donald
  */
 public interface Guarded {
+    
+    /**
+     * Indicates if the object is in an enabled state.
+     * @return {@true} if the object is in an enabled state, {@false} otherwise.
+     */
     public boolean isEnabled();
 
+    /**
+     * Sets the enabled state of the object.
+     * @param enabled {@code true} to enable the object, {@code false} to disable it. 
+     */
     public void setEnabled(boolean enabled);
+    
 }
