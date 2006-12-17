cbf8b4bff3e065ad3bdcad39f8f1a6efcd865045
Kevin Stembridge <kevin.stembridge@gmail.com>
2006-12-17 20:36:30 +0000

    Added javadoc
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@1605 817809c7-9d0e-0410-b92d-a7ac8b8adc13

--- support/src/main/java/org/springframework/richclient/command/support/ShowViewMenu.java
+++ support/src/main/java/org/springframework/richclient/command/support/ShowViewMenu.java
@@ -15,6 +15,7 @@
  */
 package org.springframework.richclient.command.support;
 
+import org.springframework.richclient.application.ApplicationServices;
 import org.springframework.richclient.application.ApplicationServicesLocator;
 import org.springframework.richclient.application.ApplicationWindow;
 import org.springframework.richclient.application.ViewDescriptor;
@@ -23,34 +24,54 @@ import org.springframework.richclient.application.config.ApplicationWindowAware;
 import org.springframework.richclient.command.CommandGroup;
 
 /**
+ * A menu containing a collection of sub-menu items that each display a given view.
+ * 
  * @author Keith Donald
  */
 public class ShowViewMenu extends CommandGroup implements ApplicationWindowAware {
 
-    private static final String ID = "showViewMenu";
+    /** The identifier of this command. */
+    public static final String ID = "showViewMenu";
 
     private ApplicationWindow window;
 
+    /**
+     * Creates a new {@code ShowViewMenu} with an id of {@value #ID}.
+     */
     public ShowViewMenu() {
         super(ID);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public void setApplicationWindow(ApplicationWindow window) {
         this.window = window;
     }
 
+    /**
+     * Called after dependencies have been set, populates this menu with action command objects 
+     * that will each show a given view when executed. The collection of 'show view' commands will
+     * be determined by querying the {@link ViewDescriptorRegistry} retrieved from 
+     * {@link ApplicationServices}. 
+     */
     public void afterPropertiesSet() {
         super.afterPropertiesSet();
+        //TODO should this be confirming that 'this.window' is not null?
         populate();
     }
 
     private void populate() {
-        ViewDescriptorRegistry viewDescriptorRegistry = (ViewDescriptorRegistry) ApplicationServicesLocator.services().getService(
-                ViewDescriptorRegistry.class);
+        ViewDescriptorRegistry viewDescriptorRegistry 
+                = (ViewDescriptorRegistry) ApplicationServicesLocator
+                                           .services()
+                                           .getService(ViewDescriptorRegistry.class);
+        
         ViewDescriptor[] views = viewDescriptorRegistry.getViewDescriptors();
         for( int i = 0; i < views.length; i++ ) {
             ViewDescriptor view = views[i];
             addInternal(view.createShowViewCommand(window));
         }
     }
+    
 }
