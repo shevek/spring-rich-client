b81d62765ce29b3a8fa47a5b98285c36658f94d1
Kevin Stembridge <kevin.stembridge@gmail.com>
2006-12-17 19:57:16 +0000

    Added javadoc
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@1603 817809c7-9d0e-0410-b92d-a7ac8b8adc13

--- support/src/main/java/org/springframework/richclient/command/support/ApplicationWindowAwareCommand.java
+++ support/src/main/java/org/springframework/richclient/command/support/ApplicationWindowAwareCommand.java
@@ -23,28 +23,53 @@ import org.springframework.richclient.application.config.ApplicationWindowAware;
 import org.springframework.richclient.command.ActionCommand;
 
 /**
+ * A skeleton implementation of an action command that needs to be aware of the
+ * {@link ApplicationWindow} in which it resides. 
+ * 
  * @author Keith Donald
  */
 public abstract class ApplicationWindowAwareCommand extends ActionCommand implements ApplicationWindowAware {
 
     private ApplicationWindow window;
 
+    /**
+     * Creates a new uninitialized {@code ApplicationWindowAwareCommand}.
+     *
+     */
     protected ApplicationWindowAwareCommand() {
-
+        //do nothing
     }
 
+    /**
+     * Creates a new {@code ApplicationWindowAwareCommand} with the given command identifier.
+     *
+     * @param commandId The identifier of this command instance. This should be unique amongst
+     * all comands within the application.
+     */
     protected ApplicationWindowAwareCommand(String commandId) {
         super(commandId);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public void setApplicationWindow(ApplicationWindow window) {
         this.window = window;
     }
 
+    /**
+     * Returns the application window that this component was created within.
+     * @return The application window, or null if this property has not yet been initialized.
+     */
     protected ApplicationWindow getApplicationWindow() {
         return window;
     }
 
+    /**
+     * Returns the {@link JFrame} of the application window that this command belongs to.
+     *
+     * @return The control component of the application window, never null.
+     */
     protected JFrame getParentWindowControl() {
         // allow subclasses to derive where the application window comes from
         final ApplicationWindow applicationWindow = getApplicationWindow();
@@ -53,4 +78,5 @@ public abstract class ApplicationWindowAwareCommand extends ActionCommand implem
         }
         return applicationWindow.getControl();
     }
+    
 }
