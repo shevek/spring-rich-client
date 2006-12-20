9923c672844b6badf9cf466b6c5fb76deceb01d3
Kevin Stembridge <kevin.stembridge@gmail.com>
2006-12-20 23:42:31 +0000

    Added javadoc
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@1612 817809c7-9d0e-0410-b92d-a7ac8b8adc13

--- support/src/main/java/org/springframework/richclient/command/ActionCommandExecutor.java
+++ support/src/main/java/org/springframework/richclient/command/ActionCommandExecutor.java
@@ -15,6 +15,14 @@
  */
 package org.springframework.richclient.command;
 
+/**
+ * This interface is to be implemented by objects that are able to execute an action command.
+ */
 public interface ActionCommandExecutor {
+    
+    /**
+     * Performs the action. 
+     */
     public void execute();
+    
 }
