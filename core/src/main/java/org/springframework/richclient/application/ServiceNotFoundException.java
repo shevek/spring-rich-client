2cbcf89f2b761e8c9a135d13058a3026ebee877d
Kevin Stembridge <kevin.stembridge@gmail.com>
2007-01-08 16:29:49 +0000

    Initial import
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@1629 817809c7-9d0e-0410-b92d-a7ac8b8adc13

---
+++ core/src/main/java/org/springframework/richclient/application/ServiceNotFoundException.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2002-2006 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.springframework.richclient.application;
+
+
+/**
+ * Indicates that an application service of a given type could not be found.
+ *
+ * @author Kevin Stembridge
+ * @since 0.3
+ *
+ */
+public class ServiceNotFoundException extends RuntimeException {
+    
+    private static final long serialVersionUID = 4732373005271272964L;
+    private final Class serviceClass;
+    
+    private static String createDefaultMessage(Class serviceClass) {
+        
+        if (serviceClass != null) {
+            return "Unable to locate an application service of type ["
+                   + serviceClass.getName()
+                   + "]";
+        } 
+        else {
+            return "Unable to locate an application service. The type of the service is either "
+                   + "unknown or was not provided when this exception was created.";
+        }
+        
+    }
+
+    /**
+     * Creates a new {@code ServiceNotFoundException}. A default message containing the name of the
+     * given class will be used. 
+     *
+     * @param serviceClass The class of the application service that could not be located.
+     */
+    public ServiceNotFoundException(Class serviceClass) {
+        super(createDefaultMessage(serviceClass));
+        this.serviceClass = serviceClass;
+    }
+    
+    /**
+     * Creates a new {@code ServiceNotFoundException} with the given detail message and nested 
+     * exception.
+     *
+     * @param message The detail message.
+     * @param serviceClass The class of the application service that could not be located.
+     * @param cause An optional nested exception that occurred attempting to locate the service.
+     */
+    public ServiceNotFoundException(String message, Class serviceClass) {
+        super(message);
+        this.serviceClass = serviceClass;
+    }
+    
+    /**
+     * Creates a new {@code ServiceNotFoundException} with the given detail message and nested 
+     * exception.
+     *
+     * @param message The detail message.
+     * @param serviceClass The class of the application service that could not be located.
+     * @param cause An optional nested exception that occurred attempting to locate the service.
+     */
+    public ServiceNotFoundException(String message, Class serviceClass, Throwable cause) {
+        super(message, cause);
+        this.serviceClass = serviceClass;
+    }
+    
+    /**
+     * Returns class of the application service that could not be located.
+     * @return Returns the service class.
+     */
+    public Class getServiceClass() {
+        return this.serviceClass;
+    }
+
+}
