f42c71eef2efafd237074abb24bec49d8598fbef
Kevin Stembridge <kevin.stembridge@gmail.com>
2007-01-08 13:01:30 +0000

    Added javadoc, simplified implementation of getViewDescriptor
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@1625 817809c7-9d0e-0410-b92d-a7ac8b8adc13

--- support/src/main/java/org/springframework/richclient/application/support/BeanFactoryViewDescriptorRegistry.java
+++ support/src/main/java/org/springframework/richclient/application/support/BeanFactoryViewDescriptorRegistry.java
@@ -17,37 +17,57 @@ package org.springframework.richclient.application.support;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.BeanNotOfRequiredTypeException;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.support.ApplicationObjectSupport;
 import org.springframework.richclient.application.ViewDescriptor;
 import org.springframework.richclient.application.ViewDescriptorRegistry;
+import org.springframework.richclient.util.Assert;
 
 
 /**
- * Simple <code>ViewRegistry</code> that pulls singleton view definitions out
- * of a spring application context.
+ * A simple {@link ViewDescriptorRegistry} implementation that pulls singleton view definitions out
+ * of a spring application context. This class is intended to be managed by a Spring IoC container.
+ * If being created programatically, be sure to call the 
+ * {@link #setApplicationContext(org.springframework.context.ApplicationContext)} method.
+ * 
  * 
  * @author Keith Donald
+ * @author Kevin Stembridge
  */
-public class BeanFactoryViewDescriptorRegistry extends ApplicationObjectSupport
-    implements ViewDescriptorRegistry {
+public class BeanFactoryViewDescriptorRegistry extends ApplicationObjectSupport implements ViewDescriptorRegistry {
 
+    /**
+     * {@inheritDoc}
+     */
     public ViewDescriptor[] getViewDescriptors() {
         Map beans = getApplicationContext().getBeansOfType(ViewDescriptor.class, false, false);
         return (ViewDescriptor[])beans.values().toArray(new ViewDescriptor[beans.size()]);
     }
 
-
+    /**
+     * Returns the view descriptor with the given identifier, or null if no such bean definition 
+     * with the given name exists in the current application context.
+     * 
+     * @param viewName The bean name of the view descriptor that is to be retrieved from the 
+     * underlying application context. Must not be null.
+     * 
+     * @throws IllegalArgumentException if {@code viewName} is null.
+     * @throws BeanNotOfRequiredTypeException if the bean retrieved from the underlying application
+     * context is not of type {@link ViewDescriptor}.
+     * 
+     */
     public ViewDescriptor getViewDescriptor(String viewName) {
-        final Object bean = getApplicationContext().getBean(viewName);
+        
+        Assert.required(viewName, "viewName");
+        
         try {
-            return (ViewDescriptor)bean;
+            return (ViewDescriptor) getApplicationContext().getBean(viewName, ViewDescriptor.class);
         }
-        catch (ClassCastException e) {
-            ClassCastException exp = new ClassCastException(bean.getClass() + " is not a " + ViewDescriptor.class +
-                " when looking up '" + viewName + "'");
-            exp.setStackTrace(e.getStackTrace());
-            throw exp;
+        catch (NoSuchBeanDefinitionException e) {
+            return null;
         }
+        
     }
 
 }
