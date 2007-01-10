0eeffe93d7b63ac0ab62858a8f96513a914cceaa
Kevin Stembridge <kevin.stembridge@gmail.com>
2007-01-10 16:10:28 +0000

    Added javadoc
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@1642 817809c7-9d0e-0410-b92d-a7ac8b8adc13

--- support/src/main/java/org/springframework/richclient/factory/ControlFactory.java
+++ support/src/main/java/org/springframework/richclient/factory/ControlFactory.java
@@ -18,9 +18,18 @@ package org.springframework.richclient.factory;
 import javax.swing.JComponent;
 
 /**
+ * A factory for producing GUI control components. 
  * 
  * @author Keith Donald
  */
 public interface ControlFactory {
+    
+    /**
+     * Returns the control that this factory is responsible for producing. Depending on the 
+     * implementation, this control may or not be a singleton instance.
+     *
+     * @return The control, never null.
+     */
     public JComponent getControl();
+    
 }
