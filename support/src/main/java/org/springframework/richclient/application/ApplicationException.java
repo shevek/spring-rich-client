aa232374d03cc905ff6a31c3d2ae7740d4dc5eb2
Kevin Stembridge <kevin.stembridge@gmail.com>
2007-01-11 21:57:59 +0000

    Added javadoc
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@1650 817809c7-9d0e-0410-b92d-a7ac8b8adc13

--- support/src/main/java/org/springframework/richclient/application/ApplicationException.java
+++ support/src/main/java/org/springframework/richclient/application/ApplicationException.java
@@ -16,17 +16,54 @@
 package org.springframework.richclient.application;
 
 /**
- * A unchecked exception indicating a error occured internal to the console.
+ * Indicates that an application-level programming error or a runtime 
+ * configuration error has occurred. 
+ * 
+ * <p>
+ * This exception should not be thrown for errors such as invalid user 
+ * input or data access errors. Basically, this represents a defect in the 
+ * program or its runtime configuration. This is a very general top-level 
+ * exception. Before creating and throwing a new instance of this type, 
+ * consider if a more specific subclass would be more appropriate.
+ * </p> 
+ * 
  */
 public class ApplicationException extends RuntimeException {
+    
+    private static final long serialVersionUID = 1732836822446152658L;
+
+    /**
+     * Creates a new {@code ApplicationException}.
+     */
+    public ApplicationException() {
+        super();
+    }
+
+    /**
+     * Creates a new {@code ApplicationException} with the specified message.
+     *
+     * @param message The detail message.
+     */
     public ApplicationException(String message) {
         super(message);
     }
 
+    /**
+     * Creates a new {@code ApplicationException} with the specified message
+     * and nested exception.
+     *
+     * @param message The detail mesage.
+     * @param cause The nested exception.
+     */
     public ApplicationException(String message, Throwable cause) {
         super(message, cause);
     }
 
+    /**
+     * Creates a new {@code ApplicationException} with the specified nested exception.
+     *
+     * @param cause The nested exception.
+     */
     public ApplicationException(Throwable cause) {
         super(cause);
     }
