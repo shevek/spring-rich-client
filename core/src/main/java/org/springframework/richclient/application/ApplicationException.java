ba5e5cab352e60523d77c5328bd43ef71f7bfb18
Kevin Stembridge <kevin.stembridge@gmail.com>
2007-01-11 22:04:01 +0000

    Moved from support module to core
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@1651 817809c7-9d0e-0410-b92d-a7ac8b8adc13

---
+++ core/src/main/java/org/springframework/richclient/application/ApplicationException.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2002-2004 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.springframework.richclient.application;
+
+/**
+ * Indicates that an application-level programming error or a runtime 
+ * configuration error has occurred. 
+ * 
+ * <p>
+ * This exception should not be thrown for errors such as invalid user 
+ * input or data access errors. Basically, this represents a defect in the 
+ * program or its runtime configuration. This is a very general top-level 
+ * exception. Before creating and throwing a new instance of this type, 
+ * consider if a more specific subclass would be more appropriate.
+ * </p> 
+ * 
+ */
+public class ApplicationException extends RuntimeException {
+    
+    private static final long serialVersionUID = 1732836822446152658L;
+
+    /**
+     * Creates a new {@code ApplicationException}.
+     */
+    public ApplicationException() {
+        super();
+    }
+
+    /**
+     * Creates a new {@code ApplicationException} with the specified message.
+     *
+     * @param message The detail message.
+     */
+    public ApplicationException(String message) {
+        super(message);
+    }
+
+    /**
+     * Creates a new {@code ApplicationException} with the specified message
+     * and nested exception.
+     *
+     * @param message The detail mesage.
+     * @param cause The nested exception.
+     */
+    public ApplicationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * Creates a new {@code ApplicationException} with the specified nested exception.
+     *
+     * @param cause The nested exception.
+     */
+    public ApplicationException(Throwable cause) {
+        super(cause);
+    }
+
+}
