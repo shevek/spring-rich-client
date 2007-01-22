f47e8a58b082bba4277c2536798ad9294b6a44fa
Mathias Broekelmann <mbroekelmann@googlemail.com>
2007-01-22 14:40:03 +0000

    moving vldocking to application package
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@1683 817809c7-9d0e-0410-b92d-a7ac8b8adc13

---
+++ vldocking/src/main/java/org/springframework/richclient/application/vldocking/VLDockingApplicationPageFactory.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2002-2004 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.springframework.richclient.application.vldocking;
+
+import org.springframework.richclient.application.ApplicationPage;
+import org.springframework.richclient.application.ApplicationPageFactory;
+import org.springframework.richclient.application.ApplicationWindow;
+import org.springframework.richclient.application.PageDescriptor;
+
+/**
+ * @author rdawes
+ *
+ */
+public class VLDockingApplicationPageFactory implements ApplicationPageFactory {
+
+	/* (non-Javadoc)
+	 * @see org.springframework.richclient.application.ApplicationPageFactory#createApplicationPage(org.springframework.richclient.application.ApplicationWindow, org.springframework.richclient.application.PageDescriptor)
+	 */
+	public ApplicationPage createApplicationPage(ApplicationWindow window, PageDescriptor descriptor) {
+		VLDockingApplicationPage page = new VLDockingApplicationPage(window, descriptor);
+        return page;
+	}
+
+}
