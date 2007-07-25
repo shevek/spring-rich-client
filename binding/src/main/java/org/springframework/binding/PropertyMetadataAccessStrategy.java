a07785b3d49155d60c22b27dff5186429ce7915f
Jan Hoskens <jh@schaubroeck.be>
2007-07-25 07:57:08 +0000

    Added javadoc
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@1786 817809c7-9d0e-0410-b92d-a7ac8b8adc13

--- binding/src/main/java/org/springframework/binding/PropertyMetadataAccessStrategy.java
+++ binding/src/main/java/org/springframework/binding/PropertyMetadataAccessStrategy.java
@@ -20,29 +20,52 @@ import java.util.Map;
 /**
  * Simple interface for accessing metadata about a particular property.
  * 
- * EXPERIMENTAL - not yet fit for general use
  * @author Keith Donald
  */
 public interface PropertyMetadataAccessStrategy {
-    public boolean isReadable(String propertyName);
 
-    public boolean isWriteable(String propertyName);
+	/**
+	 * Determine if the given property is readable.
+	 * 
+	 * @param propertyName property to examine.
+	 * @return <code>true</code> if the property is readable.
+	 */
+	boolean isReadable(String propertyName);
 
-    public Class getPropertyType(String propertyName);
+	/**
+	 * Determine if the given property is writeable.
+	 * 
+	 * @param propertyName property to examine.
+	 * @return <code>true</code> if the property is writeable.
+	 */
+	boolean isWriteable(String propertyName);
 
-    /**
-     * Returns custom metadata that may be associated with the specified
-     * property path. 
-     */
-    Object getUserMetadata(String propertyName, String key);
+	/**
+	 * Get the type of the given property.
+	 * 
+	 * @param propertyName property to examine.
+	 * @return the type of the property.
+	 */
+	Class getPropertyType(String propertyName);
 
-    /**
-     * Returns all custom metadata associated with the specified property
-     * in the form of a Map.
-     *
-     * @return Map containing String keys - this method may or may not return
-     *         <code>null</code> if there is no custom metadata associated
-     *         with the property.
-     */
-    Map getAllUserMetadata(String propertyName);
+	/**
+	 * Returns custom metadata that may be associated with the specified
+	 * property path.
+	 * 
+	 * @param propertyName property to examine.
+	 * @param key used to retreive the metadata.
+	 * @return Object stored under the given key.
+	 */
+	Object getUserMetadata(String propertyName, String key);
+
+	/**
+	 * Returns all custom metadata associated with the specified property in the
+	 * form of a Map.
+	 * 
+	 * @param propertyName property to examine.
+	 * @return Map containing String keys - this method may or may not return
+	 * <code>null</code> if there is no custom metadata associated with the
+	 * property.
+	 */
+	Map getAllUserMetadata(String propertyName);
 }
