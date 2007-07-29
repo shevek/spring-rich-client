d9ae7f0527f2ba038bb52325949bfd40905c6495
Kevin Stembridge <kevin.stembridge@gmail.com>
2007-07-29 19:18:13 +0000

    Added javadoc, no functional changes
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@1793 817809c7-9d0e-0410-b92d-a7ac8b8adc13

--- support/src/main/java/org/springframework/richclient/command/GuardedActionCommandExecutor.java
+++ support/src/main/java/org/springframework/richclient/command/GuardedActionCommandExecutor.java
@@ -20,10 +20,27 @@ import java.beans.PropertyChangeListener;
 import org.springframework.richclient.core.Guarded;
 
 /**
+ * An {@link ActionCommandExecutor} that can be enabled or disabled, with optional listeners
+ * for these state changes.
+ *  
  * @author Keith Donald
  */
 public interface GuardedActionCommandExecutor extends Guarded, ActionCommandExecutor {
+	
+	/**
+	 * Adds the given listener to the collection of listeners that will be notified 
+	 * when the command executor's enabled state changes.
+	 *
+	 * @param listener The listener to be added.
+	 */
     public void addEnabledListener(PropertyChangeListener listener);
 
+    /**
+     * Removes the given listener from the collection of listeners that will be 
+     * notified when the command executor's enabled state changes.
+     *
+     * @param listener The listener to be removed.
+     */
     public void removeEnabledListener(PropertyChangeListener listener);
+    
 }
