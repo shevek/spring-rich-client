00a47f814dcef82a3bc8baf10081a7ba31ca6162
Kevin Stembridge <kevin.stembridge@gmail.com>
2007-07-29 16:03:18 +0000

    Added javadoc, no functional changes
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@1788 817809c7-9d0e-0410-b92d-a7ac8b8adc13

--- support/src/main/java/org/springframework/richclient/command/TargetableActionCommand.java
+++ support/src/main/java/org/springframework/richclient/command/TargetableActionCommand.java
@@ -21,27 +21,57 @@ import java.beans.PropertyChangeListener;
 import org.springframework.util.ObjectUtils;
 
 /**
+ * An {@link ActionCommand} that delegates to an internal {@link ActionCommandExecutor}.
+ * The executor can be provided at construction but can also be provided 
+ * programmatically at runtime and replaced during program execution. This enables a shared
+ * global command feature whereby a single command is specified that can perform different 
+ * actions depending on the currently active context. 
+ * 
  * @author Keith Donald
  */
 public class TargetableActionCommand extends ActionCommand {
+	
     private ActionCommandExecutor commandExecutor;
 
     private PropertyChangeListener guardRelay;
 
+    /**
+     * Creates a new uninitialized {@code TargetableActionCommand}. If defined
+     * in a Spring bean factory, the name of the bean will become the id of this instance.
+     */
     public TargetableActionCommand() {
         this(null);
     }
 
+    /**
+     * Creates a new {@code TargetableActionCommand} with the given identifier. 
+     * The instance will be initialized in a disabled state.
+     *
+     * @param commandId The identifier for this instance.
+     */
     public TargetableActionCommand(String commandId) {
         this(commandId, null);
     }
 
-    public TargetableActionCommand(String commandId, ActionCommandExecutor delegate) {
+    /**
+     * Creates a new {@code TargetableActionCommand} with the given identifier and initial 
+     * executor. The instance will be initialized in a disabled state.
+     *
+     * @param commandId The identifier for this instance. 
+     * @param commandExecutor The initial command executor. 
+     */
+    public TargetableActionCommand(String commandId, ActionCommandExecutor commandExecutor) {
         super(commandId);
         setEnabled(false);
-        setCommandExecutor(delegate);
+        setCommandExecutor(commandExecutor);
     }
 
+    /**
+     * Attaches the given executor to this command instance, detaching the current executor in the process.
+     *
+     * @param commandExecutor The executor to be attached. May be null, in which case this command 
+     * will be disabled.
+     */
     public void setCommandExecutor(ActionCommandExecutor commandExecutor) {
         if (ObjectUtils.nullSafeEquals(this.commandExecutor, commandExecutor)) {
             return;
@@ -58,6 +88,11 @@ public class TargetableActionCommand extends ActionCommand {
         }
     }
 
+    /**
+     * Attaches the currently assigned command executor to this instance. The command will
+     * be enabled by default unless the executor is a {@link GuardedActionCommandExecutor}, 
+     * in which case the command will be assigned the enabled state of the executor. 
+     */
     private void attachCommandExecutor() {
         if (this.commandExecutor instanceof GuardedActionCommandExecutor) {
             GuardedActionCommandExecutor dynamicHandler = (GuardedActionCommandExecutor)commandExecutor;
@@ -68,7 +103,7 @@ public class TargetableActionCommand extends ActionCommand {
             setEnabled(true);
         }
         if (logger.isDebugEnabled()) {
-            logger.debug("Command delegate '" + this.commandExecutor + "' attached.");
+            logger.debug("Command executor '" + this.commandExecutor + "' attached.");
         }
     }
 
@@ -81,6 +116,9 @@ public class TargetableActionCommand extends ActionCommand {
         ((GuardedActionCommandExecutor)commandExecutor).addEnabledListener(guardRelay);
     }
 
+    /**
+     * Detaches the current executor from this command and sets the command to disabled state. 
+     */
     public void detachCommandExecutor() {
         if (this.commandExecutor instanceof GuardedActionCommandExecutor) {
             unsubscribeFromGuardedCommandDelegate();
@@ -94,12 +132,17 @@ public class TargetableActionCommand extends ActionCommand {
         ((GuardedActionCommandExecutor)this.commandExecutor).removeEnabledListener(guardRelay);
     }
 
+    /**
+     * Executes this command by delegating to the currently assigned executor.
+     */
     protected void doExecuteCommand() {
-        if (commandExecutor instanceof ParameterizableActionCommandExecutor) {
-            ((ParameterizableActionCommandExecutor)commandExecutor).execute(getParameters());
+        if (this.commandExecutor instanceof ParameterizableActionCommandExecutor) {
+            ((ParameterizableActionCommandExecutor) this.commandExecutor).execute(getParameters());
         }
         else {
-            commandExecutor.execute();
+        	if (this.commandExecutor != null) {
+        		this.commandExecutor.execute();
+        	}
         }
     }
 
