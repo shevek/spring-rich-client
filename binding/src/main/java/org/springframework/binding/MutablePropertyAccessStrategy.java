31cb2d700171d3989a26e40e3afc2b87d00dd2fc
Jan Hoskens <jh@schaubroeck.be>
2008-01-25 16:55:07 +0000

    Added some javadoc.
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@1915 817809c7-9d0e-0410-b92d-a7ac8b8adc13

--- binding/src/main/java/org/springframework/binding/MutablePropertyAccessStrategy.java
+++ binding/src/main/java/org/springframework/binding/MutablePropertyAccessStrategy.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2002-2004 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -24,19 +24,19 @@ import org.springframework.binding.value.ValueModel;
  * mutable operations. Specifically, this interface allows:
  * </p>
  * <ul>
- * <li>registering custom property editors for performing type conversions
+ * <li>registering custom property editors for performing type conversions</li>
  * <li>returning a domain object holder allowing the underlying domain object
- * to be changed and subscribed to for modification, and
- * <li>adding listeners for changes on particular properties.
+ * to be changed and subscribed to for modification, and</li>
+ * <li>adding listeners for changes on particular properties.</li>
  * </ul>
- * 
+ *
  * @author Keith Donald
  */
 public interface MutablePropertyAccessStrategy extends PropertyAccessStrategy {
 
 	/**
 	 * Get the <code>ValueModel</code> used to access the domainObject.
-	 * 
+	 *
 	 * @return the <code>ValueModel</code> of the domainObject.
 	 */
 	ValueModel getDomainObjectHolder();
@@ -44,7 +44,7 @@ public interface MutablePropertyAccessStrategy extends PropertyAccessStrategy {
 	/**
 	 * Get the <code>ValueModel</code> to access the given property. Possibly
 	 * creating the valueModel if needed.
-	 * 
+	 *
 	 * @param propertyPath property to access.
 	 * @return <code>ValueModel</code> that handles the given property.
 	 * @throws BeansException
@@ -52,22 +52,26 @@ public interface MutablePropertyAccessStrategy extends PropertyAccessStrategy {
 	ValueModel getPropertyValueModel(String propertyPath) throws BeansException;
 
 	/**
-	 * Get a <code>MutablePropertyAccessStrategy</code> for the given property.
-	 * 
+	 * Get a <code>MutablePropertyAccessStrategy</code> for the given
+	 * property.
+	 *
 	 * TODO check why this exists and where this is used.
-	 * 
+	 *
 	 * @param propertyPath property.
-	 * @return <code>MutablePropertyAccessStrategy</code> for the given property.
+	 * @return <code>MutablePropertyAccessStrategy</code> for the given
+	 * property.
 	 * @throws BeansException
 	 */
 	MutablePropertyAccessStrategy getPropertyAccessStrategyForPath(String propertyPath) throws BeansException;
 
 	/**
-	 * Return a new <code>MutablePropertyAccessStrategy</code> for the given valueModel.
-	 * 
+	 * Return a new <code>MutablePropertyAccessStrategy</code> for the given
+	 * valueModel.
+	 *
 	 * TODO check why this exists and where this is used.
-	 * 
-	 * @param domainObjectHolder a <code>ValueModel</code> containing the domainObject.
+	 *
+	 * @param domainObjectHolder a <code>ValueModel</code> containing the
+	 * domainObject.
 	 * @return a newly created <code>MutablePropertyAccessStrategy</code>.
 	 */
 	MutablePropertyAccessStrategy newPropertyAccessStrategy(ValueModel domainObjectHolder);
--- binding/src/main/java/org/springframework/binding/form/ConfigurableFormModel.java
+++ binding/src/main/java/org/springframework/binding/form/ConfigurableFormModel.java
@@ -18,24 +18,92 @@ package org.springframework.binding.form;
 import org.springframework.binding.value.ValueModel;
 
 /**
- * Sub-interface implemented by form models that allow for configuration
- * of the form's value models, id etc..
+ * Sub-interface implemented by form models that allow for configuration of the
+ * form's value models, id etc..
  *
  * @author Keith Donald
  * @author Oliver Hutchison
  */
 public interface ConfigurableFormModel extends FormModel {
-    public void setId(String id);
 
-    public void setEnabled(boolean enabled);
+	/**
+	 * An id to identify this formModel.
+	 *
+	 * @param id
+	 */
+	public void setId(String id);
 
-    public void setReadOnly(boolean readOnly);
+	/**
+	 * <p>
+	 * Set the enabled state of this formModel. All fieldMetaData should take
+	 * the enabled state of the formModel into account but should not alter
+	 * their own enclosed enabled state.
+	 * </p>
+	 *
+	 * <p>
+	 * A disabled formModel can be compared to a visual component which doesn't
+	 * respond to any user interaction (grey-out).
+	 * </p>
+	 *
+	 * @param enabled set to <code>true</code> if the formModel should be
+	 * enabled. Set to <code>false</code> if all fields should be disabled.
+	 */
+	public void setEnabled(boolean enabled);
 
-    public ValueModel add(String propertyName);
+	/**
+	 * <p>
+	 * Set the readOnly state of this formModel. All fieldMetaData should take
+	 * the readOnly state of the formModel into account but should not alter
+	 * their own enclosed readOnly state.
+	 * </p>
+	 *
+	 * <p>
+	 * A formModel in readOnly state can be seen as visual component in which
+	 * the user can navigate but not alter any values. (Eg editable TextFields)
+	 * </p>
+	 *
+	 * @param readOnly set to <code>true</code> if all fields should be set
+	 * readOnly.
+	 */
+	public void setReadOnly(boolean readOnly);
 
-    public ValueModel add(String propertyName, ValueModel valueModel);
+	/**
+	 * Add a valueModel for the given property.
+	 *
+	 * @param propertyName the property to create a valueModel for.
+	 * @return a ValueModel that wraps the property.
+	 */
+	public ValueModel add(String propertyName);
 
-    public ValueModel addMethod(String propertyMethodName, String derivedFromProperty);
+	/**
+	 * Add the given valueModel as wrapper for the given property. Note that the
+	 * given valueModel should be used to access the property, but may be
+	 * wrapped(chained) a number of times in other valueModels.
+	 *
+	 * @param propertyName the property.
+	 * @param valueModel the valueModel to access the property.
+	 * @return a valueModel that wraps the given valueModel.
+	 */
+	public ValueModel add(String propertyName, ValueModel valueModel);
 
-    public ValueModel addMethod(String propertyMethodName, String[] derivedFromProperties);
+	/**
+	 * Add a valueModel that holds a derived value computed by invoking the
+	 * given method with the given property as argument on the formModel.
+	 *
+	 * @param propertyMethodName method to invoke.
+	 * @param derivedFromProperty property to use as argument.
+	 * @return a valueModel holding the derived value.
+	 */
+	public ValueModel addMethod(String propertyMethodName, String derivedFromProperty);
+
+	/**
+	 * Add a valueModel that holds a derived value computed by invoking the
+	 * given method with a number of other properties as arguments on the
+	 * formModel.
+	 *
+	 * @param propertyMethodName method to invoke.
+	 * @param derivedFromProperties a number of properties to use as arguments on the method.
+	 * @return
+	 */
+	public ValueModel addMethod(String propertyMethodName, String[] derivedFromProperties);
 }
