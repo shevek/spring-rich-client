e54dc5309f3aaf768699ae97e8a7612fa86dc8d6
Peter De Bruycker <peter.de.bruycker+spring-rich@gmail.com>
2008-01-29 10:54:10 +0000

    applied patch in RCP-473: allow reuse of application pages in vldocking
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@1944 817809c7-9d0e-0410-b92d-a7ac8b8adc13

--- spring-richclient-docking/src/main/java/org/springframework/richclient/application/docking/vldocking/VLDockingApplicationPageFactory.java
+++ spring-richclient-docking/src/main/java/org/springframework/richclient/application/docking/vldocking/VLDockingApplicationPageFactory.java
@@ -19,6 +19,8 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -32,78 +34,128 @@ import org.springframework.richclient.application.PageListener;
 import com.vlsolutions.swing.docking.DockingContext;
 
 /**
+ * <tt>ApplicationPageFactory</tt> that creates instances of <tt>VLDockingApplicationPage</tt>.
+ * 
  * @author Rogan Dawes
  */
 public class VLDockingApplicationPageFactory implements ApplicationPageFactory {
 
     private static final Log logger = LogFactory.getLog(VLDockingApplicationPageFactory.class);
-    
-	/* (non-Javadoc)
-	 * @see org.springframework.richclient.application.ApplicationPageFactory#createApplicationPage(org.springframework.richclient.application.ApplicationWindow, org.springframework.richclient.application.PageDescriptor)
-	 */
-	public ApplicationPage createApplicationPage(ApplicationWindow window, PageDescriptor descriptor) {
-		VLDockingApplicationPage page = new VLDockingApplicationPage(window, descriptor);
-		
+
+    private boolean reusePages;
+    private Map pageCache = new HashMap();
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.springframework.richclient.application.ApplicationPageFactory#createApplicationPage(org.springframework.richclient.application.ApplicationWindow,
+     *      org.springframework.richclient.application.PageDescriptor)
+     */
+    public ApplicationPage createApplicationPage(ApplicationWindow window, PageDescriptor descriptor) {
+        if (reusePages) {
+            VLDockingApplicationPage page = findPage(window, descriptor);
+            if (page != null) {
+                return page;
+            }
+        }
+        VLDockingApplicationPage page = new VLDockingApplicationPage(window, descriptor);
+        if (reusePages) {
+            cachePage(page);
+        }
+
         window.addPageListener(new PageListener() {
             public void pageOpened(ApplicationPage page) {
                 // nothing to do here
             }
+
             public void pageClosed(ApplicationPage page) {
                 VLDockingApplicationPage vlDockingApplicationPage = (VLDockingApplicationPage) page;
                 saveDockingContext(vlDockingApplicationPage);
             }
         });
-		
-        return page;
-	}
-	
-	/**
-	 * Saves the docking layout of a docking page fo the application
-	 * 
-	 * @param appWindow The application window
-	 *      (needed to hook in for the docking context)
-	 */
-	private void saveDockingContext(VLDockingApplicationPage dockingPage) {
-	    DockingContext dockingContext = dockingPage.getDockingContext();
-	 
-	    // Page descriptor needed for config path
-	    VLDockingPageDescriptor vlDockingPageDescriptor = (VLDockingPageDescriptor) 
-	        Application.instance().getApplicationContext().getBean(dockingPage.getId());
-	 
-	    // Write docking context to file
-	    BufferedOutputStream buffOs = null;
-	    try {
-	        File desktopLayoutFile = vlDockingPageDescriptor.getInitialLayout().getFile();
-	        checkForConfigPath(desktopLayoutFile);
-	 
-	        buffOs = new BufferedOutputStream(new FileOutputStream(desktopLayoutFile));
-	        dockingContext.writeXML(buffOs);
-	        buffOs.close();
-	        logger.debug("Wrote docking context to config file " + desktopLayoutFile);
-	 
-	    } catch (IOException e) {
-	        logger.warn("Error writing VLDocking config", e);
-	    } finally {
-	        try { buffOs.close(); } catch (Exception e) { }
-	    }
-	}
-	 
-	/**
-	 * Creates the config directory, if it doesn't exist already
-	 * 
-	 * @param configFile The file for which to create the path
-	 */
-	private void checkForConfigPath(File configFile) {
-	    String desktopLayoutFilePath = configFile.getAbsolutePath();
-	    String configDirPath = desktopLayoutFilePath.substring(
-	            0, desktopLayoutFilePath.lastIndexOf(System.getProperty("file.separator")));
-	    File configDir = new File(configDirPath);
-	 
-	    // create config dir if it does not exist
-	    if (!configDir.exists()) {
-	        configDir.mkdirs();
-	        logger.debug("Newly created config directory");
-	    }
-	}
 
+        return page;
+    }
+
+    /**
+     * Saves the docking layout of a docking page fo the application
+     * 
+     * @param appWindow
+     *            The application window (needed to hook in for the docking context)
+     */
+    private void saveDockingContext(VLDockingApplicationPage dockingPage) {
+        DockingContext dockingContext = dockingPage.getDockingContext();
+
+        // Page descriptor needed for config path
+        VLDockingPageDescriptor vlDockingPageDescriptor = (VLDockingPageDescriptor) Application.instance()
+                .getApplicationContext().getBean(dockingPage.getId());
+
+        // Write docking context to file
+        BufferedOutputStream buffOs = null;
+        try {
+            File desktopLayoutFile = vlDockingPageDescriptor.getInitialLayout().getFile();
+            checkForConfigPath(desktopLayoutFile);
+
+            buffOs = new BufferedOutputStream(new FileOutputStream(desktopLayoutFile));
+            dockingContext.writeXML(buffOs);
+            buffOs.close();
+            logger.debug("Wrote docking context to config file " + desktopLayoutFile);
+
+        }
+        catch (IOException e) {
+            logger.warn("Error writing VLDocking config", e);
+        }
+        finally {
+            try {
+                buffOs.close();
+            }
+            catch (Exception e) {
+            }
+        }
+    }
+
+    /**
+     * Creates the config directory, if it doesn't exist already
+     * 
+     * @param configFile
+     *            The file for which to create the path
+     */
+    private void checkForConfigPath(File configFile) {
+        String desktopLayoutFilePath = configFile.getAbsolutePath();
+        String configDirPath = desktopLayoutFilePath.substring(0, desktopLayoutFilePath.lastIndexOf(System
+                .getProperty("file.separator")));
+        File configDir = new File(configDirPath);
+
+        // create config dir if it does not exist
+        if (!configDir.exists()) {
+            configDir.mkdirs();
+            logger.debug("Newly created config directory");
+        }
+    }
+
+    protected VLDockingApplicationPage findPage(ApplicationWindow window, PageDescriptor descriptor) {
+        Map pages = (Map) pageCache.get(window);
+        if (pages == null) {
+            return null;
+        }
+        
+        return (VLDockingApplicationPage) pages.get(descriptor.getId());
+    }
+
+    protected void cachePage(VLDockingApplicationPage page) {
+        Map pages = (Map) pageCache.get(page.getWindow());
+        if (pages == null) {
+            pages = new HashMap();
+            pageCache.put(page.getWindow(), pages);
+        }
+        pages.put(page.getId(), page);
+    }
+
+    /**
+     * @param reusePages
+     *            the reusePages to set
+     */
+    public void setReusePages(boolean reusePages) {
+        this.reusePages = reusePages;
+    }
 }
