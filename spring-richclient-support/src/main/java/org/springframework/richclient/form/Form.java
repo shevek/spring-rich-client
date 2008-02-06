02c82e42ddd1b7fa612783464ecfc3ccc1f56f9a
Jan Hoskens <jh@schaubroeck.be>
2008-02-06 09:23:40 +0000

    Added javadoc & formatted code Default AbstractForm constructor uses uncapitalized simpel classname as id.
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@1963 817809c7-9d0e-0410-b92d-a7ac8b8adc13

--- spring-richclient-support/src/main/java/org/springframework/richclient/form/Form.java
+++ spring-richclient-support/src/main/java/org/springframework/richclient/form/Form.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2002-2004 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -18,6 +18,7 @@ package org.springframework.richclient.form;
 
 import java.util.List;
 
+import org.springframework.binding.form.FormModel;
 import org.springframework.binding.form.ValidatingFormModel;
 import org.springframework.binding.validation.ValidationListener;
 import org.springframework.binding.value.ValueModel;
@@ -26,81 +27,155 @@ import org.springframework.richclient.dialog.Messagable;
 import org.springframework.richclient.factory.ControlFactory;
 
 /**
+ * The graphical representation of the {@link FormModel} by extending
+ * {@link ControlFactory} and providing {@link FormModel} related methods.
+ *
+ * Additional methods that link with the model are:
+ *
+ * <ul>
+ * <li>{@link #newSingleLineResultsReporter(Messagable)}: combine the
+ * validation results of the model with a messagable component to show
+ * validation messages.</li>
+ * <li>{@link #addGuarded(Guarded)}, {@link #addGuarded(Guarded, int)} and
+ * {@link #removeGuarded(Guarded)} to bind objects to the formModel state and
+ * implement a suitable reaction. This can translate in eg a save-button that
+ * will synchronize its enabled state with a dirty and no-error state in the
+ * formModel.</li>
+ * </ul>
+ *
  * @author Keith Donald
  */
 public interface Form extends ControlFactory {
-	public String getId();
 
-	public ValidatingFormModel getFormModel();
+	/**
+	 * Returns the id of this form.
+	 */
+	String getId();
 
-	public Object getFormObject();
+	/**
+	 * Returns the formModel used by the form.
+	 */
+	ValidatingFormModel getFormModel();
 
-	public void setFormObject(Object formObject);
+	/**
+	 * Convenience method to return the formObject currently used in the inner
+	 * formModel.
+	 */
+	Object getFormObject();
 
-	public Object getValue(String formProperty);
+	/**
+	 * Convenience method to set the formObject on the inner formModel.
+	 */
+	void setFormObject(Object formObject);
 
-	public ValueModel getValueModel(String formProperty);
+	/**
+	 * Convenience method to get the value of a specific property from the inner
+	 * formModel.
+	 */
+	Object getValue(String formProperty);
 
-	public void addValidationListener(ValidationListener listener);
+	/**
+	 * Convenience method to get the valueModel of a specific property from the
+	 * inner formModel.
+	 */
+	ValueModel getValueModel(String formProperty);
 
+	/**
+	 * Add a ValidationListener.
+	 */
+	void addValidationListener(ValidationListener listener);
+
+	/**
+	 * Remove a ValidationListener.
+	 */
 	public void removeValidationListener(ValidationListener listener);
 
 	/**
-	 * Create a {@link SimpleValidationResultsReporter} for this form, sending
-	 * input to the given {@link Messagable}.
-	 * 
+	 * Create a {@link ValidationResultsReporter} for this form, sending input
+	 * to the given {@link Messagable}.
+	 *
+	 * TODO check why it's specifically mentioning "singleLine" in the method
+	 * name (can be any validationResultsReporter)
+	 *
 	 * @param messageAreaPane the message receiver used by the created
 	 * resultsReporter.
 	 * @return a new ResultsReporter.
 	 */
 	public ValidationResultsReporter newSingleLineResultsReporter(Messagable messageAreaPane);
 
-	public void addGuarded(Guarded guarded);
-	
-	public void addGuarded(Guarded guarded, int mask);
-	
-	public void removeGuarded(Guarded guarded);
-	
 	/**
-	 * @return The list of ValidationResultsReporters of this Form.
+	 * Attach the given {@link Guarded} object with the default mask to the
+	 * formModel.
+	 *
+	 * @see #addGuarded(Guarded, int)
+	 * @see FormGuard
 	 */
-	public List getValidationResultsReporters();
+	public void addGuarded(Guarded guarded);
+
+	/**
+	 * Attach the given {@link Guarded} object with the specified mask to the
+	 * formModel.
+	 *
+	 * @see FormGuard
+	 */
+	public void addGuarded(Guarded guarded, int mask);
+
+	/**
+	 * Detach the {@link Guarded} object.
+	 */
+	public void removeGuarded(Guarded guarded);
+
+	/**
+	 * Returns the list of ValidationResultsReporters of this Form.
+	 */
+	List getValidationResultsReporters();
 
 	/**
 	 * Add a ValidationResultsReporter to this Form.
-	 * 
-	 * @param validationResultsReporter
 	 */
-	public void addValidationResultsReporter(ValidationResultsReporter validationResultsReporter);
+	void addValidationResultsReporter(ValidationResultsReporter validationResultsReporter);
 
 	/**
-	 * Remove a ValidationResultsReporter from this Form.
-	 * 
-	 * @param validationResultsReporter
+	 * Remove the given ValidationResultsReporter from this Form.
 	 */
-	public void removeValidationResultsReporter(ValidationResultsReporter validationResultsReporter);
+	void removeValidationResultsReporter(ValidationResultsReporter validationResultsReporter);
 
 	/**
-	 * Add a child to this Form. Models and available ResultsReporters will be
-	 * coupled as well.
-	 * 
-	 * @param form The childForm to add.
+	 * Add the given Form as a child to this Form. FormModels and other aspects
+	 * of this form must behave according to the parent-child relation.
 	 */
-	public void addChildForm(Form form);
+	void addChildForm(Form form);
 
 	/**
-	 * Remove a child from this Form. Models and available ResultsReporters will
-	 * be decoupled as well.
-	 * 
-	 * @param form The childForm to remove.
+	 * Remove the given Form as child from this Form. Parent-child relation will
+	 * be removed from their FormModels and other aspects as well.
 	 */
-	public void removeChildForm(Form form);
+	void removeChildForm(Form form);
 
-	public boolean hasErrors();
+	/**
+	 * Returns <code>true</code> if the inner <code>FormModel</code> has
+	 * errors.
+	 */
+	boolean hasErrors();
 
-	public void commit();
+	/**
+	 * Commit all values of the <code>FormModel</code>.
+	 *
+	 * @see FormModel#commit()
+	 */
+	void commit();
 
-	public void revert();
-	
-	public void reset();
+	/**
+	 * Revert the <code>FormModel</code>.
+	 *
+	 * @see FormModel#revert()
+	 */
+	void revert();
+
+	/**
+	 * Reset the <code>FormModel</code>.
+	 *
+	 * @see FormModel#reset()
+	 */
+	void reset();
 }
