faef7ca8f0aec82c627d2121b2a8f01b63904225
Jan Hoskens <jh@schaubroeck.be>
2008-02-12 14:10:55 +0000

    added javadoc
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@1969 817809c7-9d0e-0410-b92d-a7ac8b8adc13

--- spring-richclient-support/src/main/java/org/springframework/richclient/application/setup/SetupLicenseWizardPage.java
+++ spring-richclient-support/src/main/java/org/springframework/richclient/application/setup/SetupLicenseWizardPage.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2002-2004 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,78 +29,105 @@ import org.springframework.richclient.layout.GridBagLayoutBuilder;
 import org.springframework.richclient.text.HtmlPane;
 import org.springframework.richclient.util.LabelUtils;
 import org.springframework.richclient.wizard.AbstractWizardPage;
+import org.springframework.richclient.wizard.WizardPage;
 import org.springframework.util.Assert;
 import org.springframework.util.FileCopyUtils;
 
 /**
+ * A {@link WizardPage} which shows a license text and confirmation radio
+ * buttons at the bottom. The license text can be set by
+ * {@link #setLicenseTextLocation(Resource)}.
+ *
  * @author Claudio Romano
  */
 public class SetupLicenseWizardPage extends AbstractWizardPage {
-    private HtmlPane licenseTextPane;
+	/** Pane holding the license text. */
+	private HtmlPane licenseTextPane;
 
-    private Resource licenseTextLocation;
+	/** The license text. */
+	private Resource licenseTextLocation;
 
-    public SetupLicenseWizardPage() {
-        this(null);
-    }
+	/**
+	 * Default constructor. License Resource can be added later.
+	 */
+	public SetupLicenseWizardPage() {
+		this(null);
+	}
 
-    public SetupLicenseWizardPage(Resource licenseTextLocation) {
-        super("license");
-        setLicenseTextLocation(licenseTextLocation);
-    }
+	/**
+	 * Convenience constructor which sets the license resource.
+	 *
+	 * @see #setLicenseTextLocation(Resource)
+	 */
+	public SetupLicenseWizardPage(Resource licenseTextLocation) {
+		super("license");
+		setLicenseTextLocation(licenseTextLocation);
+	}
 
-    public final void setLicenseTextLocation(Resource location) {
-        this.licenseTextLocation = location;
-        if (licenseTextPane != null) {
-            updateLicenseTextPane();
-        }
-    }
+	/**
+	 * Set the {@link Resource} to use as license text.
+	 */
+	public final void setLicenseTextLocation(Resource location) {
+		this.licenseTextLocation = location;
+		if (licenseTextPane != null) {
+			updateLicenseTextPane();
+		}
+	}
 
-    protected JComponent createControl() {
-        initLicenseTextPane();
+	/**
+	 * {@inheritDoc}
+	 */
+	protected JComponent createControl() {
+		initLicenseTextPane();
 
-        ToggleCommand acceptCommand = new ToggleCommand("acceptLicenseCommand") {
-            protected void onSelection() {
-                SetupLicenseWizardPage.this.setEnabled(true);
-            }
-        };
+		ToggleCommand acceptCommand = new ToggleCommand("acceptLicenseCommand") {
+			protected void onSelection() {
+				SetupLicenseWizardPage.this.setEnabled(true);
+			}
+		};
 
-        ToggleCommand doNotAcceptCommand = new ToggleCommand("doNotAcceptLicenseCommand") {
-            protected void onSelection() {
-                SetupLicenseWizardPage.this.setEnabled(false);
-            }
-        };
-        doNotAcceptCommand.setSelected(true);
+		ToggleCommand doNotAcceptCommand = new ToggleCommand("doNotAcceptLicenseCommand") {
+			protected void onSelection() {
+				SetupLicenseWizardPage.this.setEnabled(false);
+			}
+		};
+		doNotAcceptCommand.setSelected(true);
 
-        CommandGroup.createExclusiveCommandGroup(new ToggleCommand[] { acceptCommand, doNotAcceptCommand });
+		CommandGroup.createExclusiveCommandGroup(new ToggleCommand[] { acceptCommand, doNotAcceptCommand });
 
-        GridBagLayoutBuilder formBuilder = new GridBagLayoutBuilder();
-        formBuilder.append(new JScrollPane(licenseTextPane), 1, 1, true, true);
-        formBuilder.nextLine();
-        formBuilder.append(acceptCommand.createRadioButton());
-        formBuilder.nextLine();
-        formBuilder.append(doNotAcceptCommand.createRadioButton());
-        return formBuilder.getPanel();
-    }
+		GridBagLayoutBuilder formBuilder = new GridBagLayoutBuilder();
+		formBuilder.append(new JScrollPane(licenseTextPane), 1, 1, true, true);
+		formBuilder.nextLine();
+		formBuilder.append(acceptCommand.createRadioButton());
+		formBuilder.nextLine();
+		formBuilder.append(doNotAcceptCommand.createRadioButton());
+		return formBuilder.getPanel();
+	}
 
-    protected void initLicenseTextPane() {
-        this.licenseTextPane = new HtmlPane();
-        updateLicenseTextPane();
-    }
+	/**
+	 * Create the html pane and update its contents.
+	 */
+	protected void initLicenseTextPane() {
+		this.licenseTextPane = new HtmlPane();
+		updateLicenseTextPane();
+	}
 
-    private void updateLicenseTextPane() {
-        try {
-            Assert.state(licenseTextLocation != null, "License text location is not set");
-            String text = FileCopyUtils.copyToString(new BufferedReader(new InputStreamReader(licenseTextLocation
-                    .getInputStream())));
-            licenseTextPane.setText(LabelUtils.htmlBlock(text));
-        }
-        catch (IOException e) {
-            final IllegalStateException exp =
-                    new IllegalStateException("License text not accessible: "+e.getMessage());
-            exp.setStackTrace(e.getStackTrace());
-            throw exp;
-        }
-    }
+	/**
+	 * Updates the text in the html pane.
+	 */
+	private void updateLicenseTextPane() {
+		try {
+			Assert.state(licenseTextLocation != null, "License text location is not set");
+			String text = FileCopyUtils.copyToString(new BufferedReader(new InputStreamReader(licenseTextLocation
+					.getInputStream())));
+			licenseTextPane.setText(LabelUtils.htmlBlock(text));
+		}
+		catch (IOException e) {
+			final IllegalStateException exp = new IllegalStateException("License text not accessible: "
+					+ e.getMessage());
+			exp.setStackTrace(e.getStackTrace());
+			throw exp;
+		}
+	}
 
 }
