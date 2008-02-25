6adedc4770cef672108a97383312e7d436481573
Jan Hoskens <jh@schaubroeck.be>
2008-02-25 08:25:47 +0000

    fixed getter/setter conflict (different types)
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@1974 817809c7-9d0e-0410-b92d-a7ac8b8adc13

--- spring-richclient-binding/src/main/java/org/springframework/binding/value/support/DefaultValueChangeDetector.java
+++ spring-richclient-binding/src/main/java/org/springframework/binding/value/support/DefaultValueChangeDetector.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2002-2005 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -35,9 +35,9 @@ import org.springframework.binding.value.ValueChangeDetector;
  * operator) will be used.
  * <p>
  * This is the default value change detector handed out by the Application Services.
- * 
+ *
  * @author Larry Streepy
- * 
+ *
  */
 public class DefaultValueChangeDetector implements ValueChangeDetector {
 
@@ -54,7 +54,7 @@ public class DefaultValueChangeDetector implements ValueChangeDetector {
      * requirements of this class, difference is determined using <code>!=</code>,
      * however, to improve accuracy #equals will be used when this is definitely safe e.g.
      * for Strings, Booleans, Numbers, Dates.
-     * 
+     *
      * @param oldValue Original object value
      * @param newValue New object value
      * @return true if the objects are different enough to indicate a change in the value
@@ -70,10 +70,10 @@ public class DefaultValueChangeDetector implements ValueChangeDetector {
     /**
      * Specify the set of classes that have an equals method that is safe to use for
      * determining a value change.
-     * 
+     *
      * @param classes with safe equals methods
      */
-    public void setClassesWithSafeEquals(List classes) {
+    public void setClassesWithSafeEquals(Collection classes) {
         classesWithSafeEquals.clear();
         classesWithSafeEquals.addAll( classes );
     }
