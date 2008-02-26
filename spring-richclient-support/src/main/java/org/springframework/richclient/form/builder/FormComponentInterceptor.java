62d87190c1bb7ded71be12d5592231a74efaa71b
Jan Hoskens <jh@schaubroeck.be>
2008-02-26 08:30:26 +0000

    code formatting
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@1977 817809c7-9d0e-0410-b92d-a7ac8b8adc13

--- spring-richclient-support/src/main/java/org/springframework/richclient/form/builder/FormComponentInterceptor.java
+++ spring-richclient-support/src/main/java/org/springframework/richclient/form/builder/FormComponentInterceptor.java
@@ -17,35 +17,35 @@ package org.springframework.richclient.form.builder;
 
 import javax.swing.JComponent;
 
+
 /**
+ * <p>
  * This provides a way to wrap functionality around components and labels.
- * <p />
- * 
+ * </p>
+ *
+ * <p>
  * Example uses include changing the color when validation fails, or attaching a
  * right-click popup menu.
- * 
+ * </p>
+ *
  * @author oliverh
  */
 public interface FormComponentInterceptor {
 
-    /**
-     * Perform some kind of processing on the label.
-     * 
-     * @param propertyName
-     *            the name of the property that the label is for
-     * @param label
-     *            the label to process
-     */
-    public void processLabel(String propertyName, JComponent label);
+	/**
+	 * Perform some kind of processing on the label.
+	 *
+	 * @param propertyName the name of the property that the label is for
+	 * @param label the label to process
+	 */
+	public void processLabel(String propertyName, JComponent label);
 
-    /**
-     * Perform some kind of processing on the component.
-     * 
-     * @param propertyName
-     *            the name of the property that the component is for
-     * @param component
-     *            the component to process
-     */
-    public void processComponent(String propertyName, JComponent component);
+	/**
+	 * Perform some kind of processing on the component.
+	 *
+	 * @param propertyName the name of the property that the component is for
+	 * @param component the component to process
+	 */
+	public void processComponent(String propertyName, JComponent component);
 
 }
--- spring-richclient-support/src/main/java/org/springframework/richclient/form/builder/FormComponentInterceptorFactory.java
+++ spring-richclient-support/src/main/java/org/springframework/richclient/form/builder/FormComponentInterceptorFactory.java
@@ -20,15 +20,15 @@ import org.springframework.binding.form.FormModel;
 /**
  * Factory for creating {@link FormComponentInterceptor}s for a given
  * {@link FormModel}.
- * 
+ *
  * @author oliverh
  */
 public interface FormComponentInterceptorFactory {
 
-    /**
-     * Returns a {@link FormComponentInterceptor}s for the given
-     * {@link FormModel}.
-     */
-    public FormComponentInterceptor getInterceptor(FormModel formModel);
+	/**
+	 * Returns a {@link FormComponentInterceptor}s for the given
+	 * {@link FormModel}.
+	 */
+	public FormComponentInterceptor getInterceptor(FormModel formModel);
 
 }
