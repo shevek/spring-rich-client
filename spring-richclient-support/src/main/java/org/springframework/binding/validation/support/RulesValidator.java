9b6fcfed99e4754a4e7bdf00641ccc2e36504b26
Jan Hoskens <jh@schaubroeck.be>
2008-02-26 08:32:08 +0000

    Added javadoc & formatting code
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@1978 817809c7-9d0e-0410-b92d-a7ac8b8adc13

--- spring-richclient-support/src/main/java/org/springframework/binding/validation/support/RulesValidator.java
+++ spring-richclient-support/src/main/java/org/springframework/binding/validation/support/RulesValidator.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2002-2005 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -38,174 +38,205 @@ import org.springframework.rules.reporting.MessageTranslatorFactory;
 import org.springframework.rules.reporting.ObjectNameResolver;
 import org.springframework.rules.reporting.PropertyResults;
 
+/**
+ * <p>
+ * Implementation of a {@link RichValidator} which will check the formObject
+ * against rules found in a {@link RulesSource}. This {@link RulesSource} can
+ * be specifically supplied, which allows multiple rulesSources, or can be
+ * globally defined in the Application Context. In the latter case the
+ * {@link RulesValidator} will look for the specific {@link RulesSource} type in
+ * the context.
+ * </p>
+ *
+ * <p>
+ * When validating an object, all results are cached. Any following validation
+ * of a specific property will validate that property, update the cached results
+ * accordingly and return <em>all</em> validation results of the object.
+ * </p>
+ *
+ * @author Keith Donald
+ * @author Jan Hoskens
+ */
 public class RulesValidator implements RichValidator, ObjectNameResolver {
 
-    private static final Log logger = LogFactory.getLog(RulesValidator.class);
+	private static final Log logger = LogFactory.getLog(RulesValidator.class);
 
-    private final DefaultValidationResults results = new DefaultValidationResults();
+	private final DefaultValidationResults results = new DefaultValidationResults();
 
-    private final MessageTranslator messageTranslator;
+	private final MessageTranslator messageTranslator;
 
-    private final Map validationErrors = new HashMap();
+	private final Map validationErrors = new HashMap();
 
-    private final FormModel formModel;
+	private final FormModel formModel;
 
-    private BeanValidationResultsCollector validationResultsCollector;
+	private BeanValidationResultsCollector validationResultsCollector;
 
-    private RulesSource rulesSource;
+	private RulesSource rulesSource;
 
-    private String rulesContextId = null;
+	private String rulesContextId = null;
 
-    private Class objectClass;
+	private Class objectClass;
 
-    /**
-     * Creates a RulesValidator for the given formModel. When no RulesSource is
-     * given, a default/global RulesSource is retrieved by the ApplicationServices class.
-     * 
-     * @see org.springframework.richclient.application.ApplicationServices#getRulesSource()
-     */
-    public RulesValidator(FormModel formModel) {
-        this(formModel, null);
-    }
+	/**
+	 * Creates a RulesValidator for the given formModel. When no RulesSource is
+	 * given, a default/global RulesSource is retrieved by the
+	 * ApplicationServices class.
+	 *
+	 * @see org.springframework.richclient.application.ApplicationServices#getRulesSource()
+	 */
+	public RulesValidator(FormModel formModel) {
+		this(formModel, null);
+	}
 
-    /**
-     * Create a RulesValidator which uses the supplied RulesSource on the FormModel. 
-     */
-    public RulesValidator(FormModel formModel, RulesSource rulesSource)
-    {
-        this.formModel = formModel;
-        this.rulesSource = rulesSource;
-        validationResultsCollector = new BeanValidationResultsCollector(new FormModelPropertyAccessStrategy(formModel));
-        MessageTranslatorFactory factory = (MessageTranslatorFactory) ApplicationServicesLocator.services().getService(MessageTranslatorFactory.class);
-        messageTranslator = factory.createTranslator(this);
-    }
-    
-    public ValidationResults validate(Object object) {
-        return validate(object, null);
-    }
+	/**
+	 * Create a RulesValidator which uses the supplied RulesSource on the
+	 * FormModel.
+	 */
+	public RulesValidator(FormModel formModel, RulesSource rulesSource) {
+		this.formModel = formModel;
+		this.rulesSource = rulesSource;
+		validationResultsCollector = new BeanValidationResultsCollector(new FormModelPropertyAccessStrategy(formModel));
+		MessageTranslatorFactory factory = (MessageTranslatorFactory) ApplicationServicesLocator.services().getService(
+				MessageTranslatorFactory.class);
+		messageTranslator = factory.createTranslator(this);
+	}
 
-    public ValidationResults validate(Object object, String propertyName) {
-        // Forms can have different types of objects, so when type of object
-        // changes, messages that are already listed on the previous type must
-        // be removed.
-        if (objectClass != null && objectClass != object.getClass()) {
-            clearMessages();
-        }
-        objectClass = object.getClass();
-        Rules rules = null;
-        if (object instanceof PropertyConstraintProvider) {
-            PropertyConstraint validationRule = ((PropertyConstraintProvider)object).getPropertyConstraint(propertyName);
-            if (validationRule != null) {
-                checkRule(validationRule);
-            }
-        }
-        else {
-            if (getRulesSource() != null) {
-                rules = getRulesSource().getRules(objectClass, getRulesContextId());
-                if (rules != null) {
-                    for (Iterator i = rules.iterator(); i.hasNext();) {
-                        PropertyConstraint validationRule = (PropertyConstraint)i.next();
-                        if (propertyName == null) {
-                            if (formModel.hasValueModel(validationRule.getPropertyName())) {
-                                checkRule(validationRule);
-                            }
-                        }
-                        else if (validationRule.isDependentOn(propertyName)) {
-                            checkRule(validationRule);
-                        }
-                    }
-                }
-            }
-            else {
-                logger.debug("No rules source has been configured; "
-                        + "please set a valid reference to enable rules-based validation.");
-            }
-        }
-        return results;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	public ValidationResults validate(Object object) {
+		return validate(object, null);
+	}
 
-    private void checkRule(PropertyConstraint validationRule) {
-        BeanValidationResultsCollector resultsCollector = takeResultsCollector();
-        PropertyResults results = resultsCollector.collectPropertyResults(validationRule);
-        returnResultsCollector(resultsCollector);
-        if (results == null) {
-            constraintSatisfied(validationRule);
-        }
-        else {
-            constraintViolated(validationRule, results);
-        }
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	public ValidationResults validate(Object object, String propertyName) {
+		// Forms can have different types of objects, so when type of object
+		// changes, messages that are already listed on the previous type must
+		// be removed. If evaluating the whole object (propertyName == null)
+		// also clear results.
+		if ((propertyName == null) || ((objectClass != null) && objectClass != object.getClass())) {
+			clearMessages();
+		}
+		objectClass = object.getClass();
+		Rules rules = null;
+		if (object instanceof PropertyConstraintProvider) {
+			PropertyConstraint validationRule = ((PropertyConstraintProvider) object)
+					.getPropertyConstraint(propertyName);
+			if (validationRule != null) {
+				checkRule(validationRule);
+			}
+		}
+		else {
+			if (getRulesSource() != null) {
+				rules = getRulesSource().getRules(objectClass, getRulesContextId());
+				if (rules != null) {
+					for (Iterator i = rules.iterator(); i.hasNext();) {
+						PropertyConstraint validationRule = (PropertyConstraint) i.next();
+						if (propertyName == null) {
+							if (formModel.hasValueModel(validationRule.getPropertyName())) {
+								checkRule(validationRule);
+							}
+						}
+						else if (validationRule.isDependentOn(propertyName)) {
+							checkRule(validationRule);
+						}
+					}
+				}
+			}
+			else {
+				logger.debug("No rules source has been configured; "
+						+ "please set a valid reference to enable rules-based validation.");
+			}
+		}
+		return results;
+	}
 
-    protected void constraintSatisfied(PropertyConstraint exp) {
-        ValidationMessage message = (ValidationMessage)validationErrors.remove(exp);
-        if (message != null) {
-            results.removeMessage(message);
-        }
-    }
+	private void checkRule(PropertyConstraint validationRule) {
+		BeanValidationResultsCollector resultsCollector = takeResultsCollector();
+		PropertyResults results = resultsCollector.collectPropertyResults(validationRule);
+		returnResultsCollector(resultsCollector);
+		if (results == null) {
+			constraintSatisfied(validationRule);
+		}
+		else {
+			constraintViolated(validationRule, results);
+		}
+	}
 
-    protected void constraintViolated(PropertyConstraint exp, PropertyResults propertyResults) {
-        ValidationMessage message = new DefaultValidationMessage(exp.getPropertyName(), Severity.ERROR,
-                messageTranslator.getMessage(propertyResults));
+	protected void constraintSatisfied(PropertyConstraint exp) {
+		ValidationMessage message = (ValidationMessage) validationErrors.remove(exp);
+		if (message != null) {
+			results.removeMessage(message);
+		}
+	}
 
-        ValidationMessage oldMessage = (ValidationMessage)validationErrors.get(exp);
-        if (!message.equals(oldMessage)) {
-            results.removeMessage(oldMessage);
-            validationErrors.put(exp, message);
-            results.addMessage(message);
-        }
-    }
+	protected void constraintViolated(PropertyConstraint exp, PropertyResults propertyResults) {
+		ValidationMessage message = new DefaultValidationMessage(exp.getPropertyName(), Severity.ERROR,
+				messageTranslator.getMessage(propertyResults));
 
-    private RulesSource getRulesSource() {
-        if (rulesSource == null) {
-            rulesSource = (RulesSource)ApplicationServicesLocator.services().getService(RulesSource.class);
-        }
-        return rulesSource;
-    }
+		ValidationMessage oldMessage = (ValidationMessage) validationErrors.get(exp);
+		if (!message.equals(oldMessage)) {
+			results.removeMessage(oldMessage);
+			validationErrors.put(exp, message);
+			results.addMessage(message);
+		}
+	}
 
-    private BeanValidationResultsCollector takeResultsCollector() {
-        BeanValidationResultsCollector resultsCollector = validationResultsCollector;
-        if (resultsCollector != null) {
-            validationResultsCollector = null;
-        }
-        else {
-            resultsCollector = new BeanValidationResultsCollector(new FormModelPropertyAccessStrategy(formModel));
-        }
-        return resultsCollector;
-    }
+	private RulesSource getRulesSource() {
+		if (rulesSource == null) {
+			rulesSource = (RulesSource) ApplicationServicesLocator.services().getService(RulesSource.class);
+		}
+		return rulesSource;
+	}
 
-    private void returnResultsCollector(BeanValidationResultsCollector resultsCollector) {
-        validationResultsCollector = resultsCollector;
-    }
+	private BeanValidationResultsCollector takeResultsCollector() {
+		BeanValidationResultsCollector resultsCollector = validationResultsCollector;
+		if (resultsCollector != null) {
+			validationResultsCollector = null;
+		}
+		else {
+			resultsCollector = new BeanValidationResultsCollector(new FormModelPropertyAccessStrategy(formModel));
+		}
+		return resultsCollector;
+	}
 
-    /**
-     * Get the rules context id set on this validator.
-     * @return rules context id
-     */
-    public String getRulesContextId() {
-        return rulesContextId;
-    }
+	private void returnResultsCollector(BeanValidationResultsCollector resultsCollector) {
+		validationResultsCollector = resultsCollector;
+	}
 
-    /**
-     * Set the rules context id.  This is passed in the call to
-     * {@link RulesSource#getRules(Class, String)} to allow for context specific rules.
-     * @param rulesContextId
-     */
-    public void setRulesContextId(String rulesContextId) {
-        this.rulesContextId = rulesContextId;
-    }
+	/**
+	 * Returns the rules context id set on this validator.
+	 */
+	public String getRulesContextId() {
+		return rulesContextId;
+	}
 
+	/**
+	 * Set the rules context id. This is passed in the call to
+	 * {@link RulesSource#getRules(Class, String)} to allow for context specific
+	 * rules.
+	 * @param rulesContextId
+	 */
+	public void setRulesContextId(String rulesContextId) {
+		this.rulesContextId = rulesContextId;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
 	public String resolveObjectName(String objectName) {
 		return formModel.getFieldFace(objectName).getDisplayName();
 	}
-    
-    /**
-     * Clear the current validationMessages and the errors.
-     * 
-     * @see #validate(Object, String)
-     */
-    public void clearMessages()
-    {
-        this.results.clearMessages();
-        this.validationErrors.clear();
-    }
+
+	/**
+	 * Clear the current validationMessages and the errors.
+	 *
+	 * @see #validate(Object, String)
+	 */
+	public void clearMessages() {
+		this.results.clearMessages();
+		this.validationErrors.clear();
+	}
 }
--- spring-richclient-support/src/main/java/org/springframework/rules/PropertyConstraintProvider.java
+++ spring-richclient-support/src/main/java/org/springframework/rules/PropertyConstraintProvider.java
@@ -18,8 +18,15 @@ package org.springframework.rules;
 import org.springframework.rules.constraint.property.PropertyConstraint;
 
 /**
+ * Interface indicating the providing of validation. This can be
+ * used in the validator to identify the source for validation rules.
+ *
  * @author Keith Donald
  */
 public interface PropertyConstraintProvider {
+
+	/**
+	 * Returns the constraint on the given property.
+	 */
 	public PropertyConstraint getPropertyConstraint(String propertyName);
 }
