6f01085567876a3167879e43509ebefe98959613
Jan Hoskens <jh@schaubroeck.be>
2008-03-03 18:05:57 +0000

    Removed FormModel.validate(): no explicit validate call should be needed, initial validation state must be taken into account and any change to the valueModel will trigger an additional validation.
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@1983 817809c7-9d0e-0410-b92d-a7ac8b8adc13

--- spring-richclient-sandbox/src/main/java/org/springframework/richclient/form/AbstractMasterForm.java
+++ spring-richclient-sandbox/src/main/java/org/springframework/richclient/form/AbstractMasterForm.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2002-2005 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -605,7 +605,6 @@ public abstract class AbstractMasterForm extends AbstractForm {
         validationResultsModel.add(getFormModel().getValidationResults());
         validationResultsModel.add(getDetailFormModel().getValidationResults());
         ValidationResultsReporter reporter = new SimpleValidationResultsReporter(validationResultsModel, messageReceiver);
-        getDetailFormModel().validate();
         return reporter;
     }
 
