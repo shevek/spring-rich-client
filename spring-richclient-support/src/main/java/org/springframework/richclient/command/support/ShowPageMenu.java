28a32add9ffd62983d1a191b8aef55c7f99925fa
Jan Hoskens <jh@schaubroeck.be>
2008-03-16 10:01:57 +0000

    code formatting
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@2017 817809c7-9d0e-0410-b92d-a7ac8b8adc13

--- spring-richclient-support/src/main/java/org/springframework/richclient/command/support/ShowPageMenu.java
+++ spring-richclient-support/src/main/java/org/springframework/richclient/command/support/ShowPageMenu.java
@@ -22,57 +22,56 @@ import org.springframework.richclient.application.PageDescriptor;
 import org.springframework.richclient.application.PageDescriptorRegistry;
 import org.springframework.richclient.application.config.ApplicationWindowAware;
 import org.springframework.richclient.command.CommandGroup;
+import org.springframework.util.Assert;
 
 /**
- * A menu containing a collection of sub-menu items that each display a given page.
+ * A menu containing a collection of sub-menu items that each display a given
+ * page.
  *
  * @author Keith Donald
  * @author Rogan Dawes
  */
 public class ShowPageMenu extends CommandGroup implements ApplicationWindowAware {
 
-    /** The identifier of this command. */
-    public static final String ID = "showPageMenu";
+	/** The identifier of this command. */
+	public static final String ID = "showPageMenu";
 
-    private ApplicationWindow window;
+	private ApplicationWindow window;
 
-    /**
-     * Creates a new {@code ShowPageMenu} with an id of {@value #ID}.
-     */
-    public ShowPageMenu() {
-        super(ID);
-    }
+	/**
+	 * Creates a new {@code ShowPageMenu} with an id of {@value #ID}.
+	 */
+	public ShowPageMenu() {
+		super(ID);
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    public void setApplicationWindow(ApplicationWindow window) {
-        this.window = window;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	public void setApplicationWindow(ApplicationWindow window) {
+		this.window = window;
+	}
 
-    /**
-     * Called after dependencies have been set, populates this menu with action command objects
-     * that will each show a given page when executed. The collection of 'show page' commands will
-     * be determined by querying the {@link PageDescriptorRegistry} retrieved from
-     * {@link ApplicationServices}.
-     */
-    public void afterPropertiesSet() {
-        super.afterPropertiesSet();
-        //TODO should this be confirming that 'this.window' is not null?
-        populate();
-    }
+	/**
+	 * Called after dependencies have been set, populates this menu with action
+	 * command objects that will each show a given page when executed. The
+	 * collection of 'show page' commands will be determined by querying the
+	 * {@link PageDescriptorRegistry} retrieved from {@link ApplicationServices}.
+	 */
+	public void afterPropertiesSet() {
+		super.afterPropertiesSet();
+		Assert.notNull(window, "Application window cannot be null.");
+		populate();
+	}
 
-    private void populate() {
-        PageDescriptorRegistry pageDescriptorRegistry
-                = (PageDescriptorRegistry) ApplicationServicesLocator
-                                           .services()
-                                           .getService(PageDescriptorRegistry.class);
+	private void populate() {
+		PageDescriptorRegistry pageDescriptorRegistry = (PageDescriptorRegistry) ApplicationServicesLocator.services()
+				.getService(PageDescriptorRegistry.class);
 
-        PageDescriptor[] pages = pageDescriptorRegistry.getPageDescriptors();
-        for( int i = 0; i < pages.length; i++ ) {
-            PageDescriptor page = pages[i];
-            addInternal(page.createShowPageCommand(window));
-        }
-    }
-
+		PageDescriptor[] pages = pageDescriptorRegistry.getPageDescriptors();
+		for (int i = 0; i < pages.length; i++) {
+			PageDescriptor page = pages[i];
+			addInternal(page.createShowPageCommand(window));
+		}
+	}
 }
