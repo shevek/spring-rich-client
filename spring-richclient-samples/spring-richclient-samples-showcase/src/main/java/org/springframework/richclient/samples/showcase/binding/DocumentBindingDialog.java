9dde80d5e1f157af5bf63e02fbfede637d155e5f
Jan Hoskens <jh@schaubroeck.be>
2008-08-05 09:34:17 +0000

    Added documentBinding and shuttleList samples
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@2069 817809c7-9d0e-0410-b92d-a7ac8b8adc13

---
+++ spring-richclient-samples/spring-richclient-samples-showcase/src/main/java/org/springframework/richclient/samples/showcase/binding/DocumentBindingDialog.java
@@ -0,0 +1,132 @@
+package org.springframework.richclient.samples.showcase.binding;
+
+import org.springframework.richclient.dialog.TitledApplicationDialog;
+import org.springframework.richclient.form.AbstractForm;
+import org.springframework.richclient.form.FormModelHelper;
+import org.springframework.richclient.form.binding.swing.text.DocumentBinder;
+import org.springframework.richclient.form.binding.swing.text.RegExDocumentFactory;
+import org.springframework.richclient.form.builder.TableFormBuilder;
+import org.springframework.richclient.command.ActionCommand;
+import org.springframework.richclient.command.config.CommandConfigurer;
+import org.springframework.richclient.application.ApplicationServicesLocator;
+
+import javax.swing.*;
+
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.CellConstraints;
+
+import java.util.Collections;
+
+/**
+ * This dialog shows how the {@link org.springframework.richclient.form.binding.swing.text.DocumentBinder} can be used to create
+ * a binding with a specific {@link javax.swing.text.Document} as model behind the {@link javax.swing.JTextField}.
+ *
+ * @author Jan Hoskens
+ */
+public class DocumentBindingDialog extends TitledApplicationDialog {
+    
+    class StringValues {
+
+        String regExp = "[0-9A-z]";
+
+        boolean convertToUppercase = false;
+
+        String value;
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+
+        public String getRegExp() {
+            return regExp;
+        }
+
+        public void setRegExp(String regExp) {
+            this.regExp = regExp;
+        }
+
+        public boolean isConvertToUppercase() {
+            return convertToUppercase;
+        }
+
+        public void setConvertToUppercase(boolean convertToUppercase) {
+            this.convertToUppercase = convertToUppercase;
+        }
+
+    }
+
+    private class ParentForm extends AbstractForm {
+        private final JPanel childPanel = new JPanel(new FormLayout("fill:default:grow","fill:default:grow"));
+
+        public ParentForm() {
+			super(FormModelHelper.createFormModel(new StringValues()));
+		}
+
+		@Override
+		protected JComponent createFormControl() {
+            JPanel panel = new JPanel(new FormLayout("fill:default:grow","default, 4dlu, default, 4dlu, 30dlu:grow"));
+            TableFormBuilder builder = new TableFormBuilder(getBindingFactory());
+            builder.add("regExp");
+            builder.row();
+            builder.add("convertToUppercase");
+            CellConstraints cc = new CellConstraints();
+            panel.add(builder.getForm(), cc.xy(1,1));
+            panel.add(createBuildBindingCommand().createButton(), cc.xy(1,3));
+            panel.add(childPanel, cc.xy(1,5));
+            return panel;
+		}
+
+        private ActionCommand createBuildBindingCommand() {
+            ActionCommand actionCommand = new ActionCommand("buildBinding"){
+                protected void doExecuteCommand() {
+                    String pattern = (String)getValue("regExp");
+                    boolean upperCaseOnly = (Boolean) getValue("convertToUppercase");
+                    CellConstraints cc = new CellConstraints();
+                    ChildForm form = new ChildForm(pattern, upperCaseOnly);
+                    childPanel.removeAll();
+                    childPanel.add(form.getControl(), cc.xy(1,1));
+                    childPanel.revalidate();
+
+                }
+            };
+            ((CommandConfigurer) ApplicationServicesLocator.services().getService(CommandConfigurer.class))
+                    .configure(actionCommand);
+            return actionCommand;
+        }
+
+        private class ChildForm extends AbstractForm {
+
+            final String pattern;
+
+            final boolean upperCaseOnly;
+
+            public ChildForm(String pattern, boolean  upperCaseOnly) {
+                super(FormModelHelper.createFormModel(new StringValues()));
+                this.pattern = pattern;
+                this.upperCaseOnly = upperCaseOnly;
+            }
+
+            protected JComponent createFormControl() {
+                TableFormBuilder builder = new TableFormBuilder(getBindingFactory());
+                DocumentBinder binder = new DocumentBinder();
+                binder.setDocumentFactory(new RegExDocumentFactory(pattern, upperCaseOnly));
+                builder.add(binder.bind(getFormModel(), "value", Collections.emptyMap()), "rowSpec=fill:default:grow");
+                return builder.getForm();
+            }
+        }
+    }
+
+	@Override
+	protected JComponent createTitledDialogContentPane() {
+		return (new ParentForm()).getControl();
+	}
+
+	@Override
+	protected boolean onFinish() {
+		return true;
+	}
+}
---
+++ spring-richclient-samples/spring-richclient-samples-showcase/src/main/java/org/springframework/richclient/samples/showcase/binding/ShuttleListBindingDialog.java
@@ -0,0 +1,76 @@
+package org.springframework.richclient.samples.showcase.binding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JComponent;
+
+import org.springframework.richclient.dialog.TitledApplicationDialog;
+import org.springframework.richclient.form.AbstractForm;
+import org.springframework.richclient.form.FormModelHelper;
+import org.springframework.richclient.form.binding.swing.SandboxSwingBindingFactory;
+import org.springframework.richclient.form.builder.TableFormBuilder;
+
+/**
+ * This dialog shows a ShuttleList in which several items can be selected and added to a list on the backing formObject.
+ * Mind that this is far from perfect, but does illustrate the binding and the specific component that is created.
+ *
+ * @see org.springframework.richclient.components.ShuttleList
+ * @see org.springframework.richclient.components.ShuttleListBinder
+ */
+public class ShuttleListBindingDialog extends TitledApplicationDialog {
+
+    /** The fixed set of regions that can be selected. */
+    private List<String> selectableRegions;
+
+    public ShuttleListBindingDialog() {
+        selectableRegions = new ArrayList<String>();
+        selectableRegions.add("Land of the Dead");
+        selectableRegions.add("My little Pony Heaven");
+        selectableRegions.add("Bikini World");
+        selectableRegions.add("Paradise City");
+        selectableRegions.add("The Big Banana");
+    }
+
+    /** Value object with a list of regions that is a subselection of a given set. */
+    private class Values {
+
+        private List<String> selectedRegions;
+
+        public List<String> getSelectedRegions() {
+            return selectedRegions;
+        }
+
+        public void setSelectedRegions(List<String> selectedRegions) {
+            this.selectedRegions = selectedRegions;
+        }
+    }
+
+    private class ShuttleListForm extends AbstractForm {
+
+        public ShuttleListForm() {
+            super(FormModelHelper.createFormModel(new Values()));
+        }
+
+        @Override
+        protected JComponent createFormControl() {
+            SandboxSwingBindingFactory bindingFactory = new SandboxSwingBindingFactory(getFormModel());
+            TableFormBuilder builder = new TableFormBuilder(bindingFactory);
+            builder.setLabelAttributes("colGrId=label colSpec=left:pref rowSpec=top:pref");
+            builder.add(bindingFactory.createBoundShuttleList("selectedRegions", selectableRegions));
+            return builder.getForm();
+        }
+
+    }
+
+    @Override
+    protected JComponent createTitledDialogContentPane() {
+        return (new ShuttleListForm()).getControl();
+    }
+
+    @Override
+    protected boolean onFinish() {
+        return true;
+    }
+
+}
