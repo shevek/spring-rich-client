52ffe3f6fdcb30e0a943f3dbec6f9252df73bee4
Peter De Bruycker <peter.de.bruycker+spring-rich@gmail.com>
2008-11-21 09:28:55 +0000

    fixed package declarations
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@2124 817809c7-9d0e-0410-b92d-a7ac8b8adc13

--- spring-richclient-integrations/spring-richclient-swingdocking/src/main/java/org/springframework/richclient/application/docking/swingdocking/SwingDockingApplicationPage.java
+++ spring-richclient-integrations/spring-richclient-swingdocking/src/main/java/org/springframework/richclient/application/docking/swingdocking/SwingDockingApplicationPage.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package org.springframework.richclient.application.docking.swing;
+package org.springframework.richclient.application.docking.swingdocking;
 
 import javax.swing.JDesktopPane;
 
