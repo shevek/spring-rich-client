bc92c682f6cd69c7a7798d7e72fbfc57c70356a3
Peter De Bruycker <peter.de.bruycker+spring-rich@gmail.com>
2008-11-21 10:22:06 +0000

    added simple ViewDescriptorRegistry implementation for easier testing
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@2128 817809c7-9d0e-0410-b92d-a7ac8b8adc13

---
+++ spring-richclient-core/src/main/java/org/springframework/richclient/application/support/SimpleViewDescriptorRegistry.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2008 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.springframework.richclient.application.support;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.richclient.application.ViewDescriptor;
+import org.springframework.richclient.application.ViewDescriptorRegistry;
+import org.springframework.util.Assert;
+
+/**
+ * Very simple {@link ViewDescriptorRegistry} implementation, mostly for testing purposes.
+ * 
+ * @author Peter De Bruycker
+ */
+public class SimpleViewDescriptorRegistry implements ViewDescriptorRegistry {
+
+    private Map<String, ViewDescriptor> descriptors = new HashMap<String, ViewDescriptor>();
+
+    public SimpleViewDescriptorRegistry() {
+
+    }
+
+    public SimpleViewDescriptorRegistry(List<ViewDescriptor> descriptors) {
+        Assert.notNull(descriptors, "descriptors cannot be null");
+
+        for (ViewDescriptor descriptor : descriptors) {
+            addViewDescriptor(descriptor);
+        }
+    }
+
+    public void addViewDescriptor(ViewDescriptor descriptor) {
+        Assert.notNull(descriptor, "descriptor cannot be null");
+
+        descriptors.put(descriptor.getId(), descriptor);
+    }
+
+    public void removeViewDescriptor(ViewDescriptor descriptor) {
+        Assert.notNull(descriptor, "descriptor cannot be null");
+
+        descriptors.remove(descriptor.getId());
+    }
+
+    public ViewDescriptor getViewDescriptor(String viewDescriptorId) {
+        Assert.hasText(viewDescriptorId, "viewDescriptorId cannot be empty");
+
+        return descriptors.get(viewDescriptorId);
+    }
+
+    public ViewDescriptor[] getViewDescriptors() {
+        return descriptors.values().toArray(new ViewDescriptor[descriptors.size()]);
+    }
+
+}
