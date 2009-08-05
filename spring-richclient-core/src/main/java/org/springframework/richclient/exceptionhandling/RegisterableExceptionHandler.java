ea8431d080eb07fe7735d7bb7bb097dc3b10f785
Geoffrey De Smet <ge0ffrey.spam@gmail.com>
2009-08-05 11:58:12 +0000

    implements UncaughtExceptionHandler now that it's at least java 1.5
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@2212 817809c7-9d0e-0410-b92d-a7ac8b8adc13

--- spring-richclient-core/src/main/java/org/springframework/richclient/exceptionhandling/RegisterableExceptionHandler.java
+++ spring-richclient-core/src/main/java/org/springframework/richclient/exceptionhandling/RegisterableExceptionHandler.java
@@ -3,14 +3,11 @@ package org.springframework.richclient.exceptionhandling;
 /**
  * An exception handler which can be registered (to for example the EDT and all threads).
  *
- * @TODO extend Thread.UncaughtExceptionHandler when Spring-richclient is minimum java 1.5
  * @author Geoffrey De Smet
  * @since 0.3
  */
-public interface RegisterableExceptionHandler {
+public interface RegisterableExceptionHandler extends Thread.UncaughtExceptionHandler {
 
     void registerExceptionHandler();
-
-    void uncaughtException(Thread thread, Throwable throwable);
 
 }
