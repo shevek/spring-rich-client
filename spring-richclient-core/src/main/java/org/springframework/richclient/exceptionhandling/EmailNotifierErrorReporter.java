27f74fd5849cf0117caf3bde1657e3606444c11b
Geoffrey De Smet <ge0ffrey.spam@gmail.com>
2009-08-11 14:41:01 +0000

    EmailNotifierErrorReporter: fallback with no id on "emailNotifierErrorReporter" didn't work because setBeanName is called even for anonymous beans. When native JDIC jar are missing a decent error message should be shown.
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@2224 817809c7-9d0e-0410-b92d-a7ac8b8adc13

--- spring-richclient-core/src/main/java/org/springframework/richclient/exceptionhandling/EmailNotifierErrorReporter.java
+++ spring-richclient-core/src/main/java/org/springframework/richclient/exceptionhandling/EmailNotifierErrorReporter.java
@@ -3,6 +3,9 @@ package org.springframework.richclient.exceptionhandling;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import java.sql.SQLException;
 
 import org.apache.commons.lang.StringUtils;
 import org.jdesktop.jdic.desktop.Desktop;
@@ -13,7 +16,10 @@ import org.jdesktop.swingx.error.ErrorReporter;
 import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.context.MessageSourceResolvable;
 import org.springframework.richclient.application.ApplicationServicesLocator;
+import org.springframework.core.ErrorCoded;
 
 /**
  * <p>
@@ -55,25 +61,36 @@ public class EmailNotifierErrorReporter implements ErrorReporter, BeanNameAware,
 
     private String id;
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setBeanName(String beanName) {
+        if (getId() == null) {
+            setId(beanName);
+        }
+    }
+
     public void afterPropertiesSet() {
         if (messageSourceAccessor == null) {
             messageSourceAccessor = (MessageSourceAccessor) ApplicationServicesLocator.services().getService(
                     MessageSourceAccessor.class);
-        }
-        if (getId() == null) {
-            setId(StringUtils.uncapitalize(getClass().getSimpleName()));
         }
     }
 
     public void reportError(ErrorInfo info) {
         Message mail = new Message();
 
-        String adresses = messageSourceAccessor.getMessage(getId() + ".mailTo", "");
+        String adresses = messageSourceAccessor.getMessage(getMessageSourceResolvable(".mailTo"));
         if (!StringUtils.isEmpty(adresses)) {
             mail.setToAddrs(Arrays.<String>asList(adresses.split(";")));
         }
 
-        String subject = messageSourceAccessor.getMessage(getId() + ".title", ""); // TODO .title rename to .subject
+        String subject = messageSourceAccessor.getMessage(getMessageSourceResolvable(".title")); // TODO .title rename to .subject
         mail.setSubject(subject);
 
         Object params[] = new Object[] {
@@ -86,15 +103,30 @@ public class EmailNotifierErrorReporter implements ErrorReporter, BeanNameAware,
                 getStackTraceString(info.getErrorException())
             };
         }
-        String body = messageSourceAccessor.getMessage(getId() + ".body", params, "");
+        String body = messageSourceAccessor.getMessage(getMessageSourceResolvable(".body", params));
         mail.setBody(body);
 
         try {
             Desktop.mail(mail);
+        } catch (UnsatisfiedLinkError e) {
+            String message = messageSourceAccessor.getMessage(getMessageSourceResolvable(".noNativeJdic"));
+            throw new IllegalStateException(message, e);
         } catch (DesktopException e) {
-            String mailExceptionMessage = messageSourceAccessor.getMessage(getId() + ".mailException", "");
-            throw new RuntimeException(mailExceptionMessage, e);
+            String message = messageSourceAccessor.getMessage(getMessageSourceResolvable(".mailException"));
+            throw new IllegalStateException(message, e);
         }
+    }
+
+    protected MessageSourceResolvable getMessageSourceResolvable(String keySuffix) {
+        return getMessageSourceResolvable(keySuffix, null);
+    }
+
+    protected MessageSourceResolvable getMessageSourceResolvable(String keySuffix, Object[] params) {
+        List<String> messageKeyList = new ArrayList<String>();
+        messageKeyList.add(getId() + keySuffix);
+        messageKeyList.add("emailNotifierErrorReporter" + keySuffix);
+        String[] messagesKeys = messageKeyList.toArray(new String[messageKeyList.size()]);
+        return new DefaultMessageSourceResolvable(messagesKeys, params, messagesKeys[0]);
     }
 
     protected String getStackTraceString(Throwable t) {
@@ -104,20 +136,6 @@ public class EmailNotifierErrorReporter implements ErrorReporter, BeanNameAware,
         pw.flush();
         sw.flush();
         return sw.toString();
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public void setBeanName(String name) {
-        if (getId() == null) {
-            setId(name);
-        }
     }
 
 }
