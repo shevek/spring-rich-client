112fe04361b2e7c7077d7246701a6f75dd514805
Geoffrey De Smet <ge0ffrey.spam@gmail.com>
2009-09-14 07:57:40 +0000

    RCP-604 make code readable: rename TabMessages to MessagebleTab, messages to messableTabs/messages, format code, use Map instead HashMap, ...
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@2234 817809c7-9d0e-0410-b92d-a7ac8b8adc13

--- spring-richclient-core/src/main/java/org/springframework/richclient/components/MessagableTabbedPane.java
+++ spring-richclient-core/src/main/java/org/springframework/richclient/components/MessagableTabbedPane.java
@@ -10,12 +10,12 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Stack;
+import java.util.Map;
 import java.awt.*;
 
-public class MessagableTabbedPane extends JTabbedPane implements MessagableTab
-{
+public class MessagableTabbedPane extends JTabbedPane implements MessagableTab {
 
-    private List<TabMessages> messages = new ArrayList<TabMessages>();
+    private List<MessagableTab> messagableTabs = new ArrayList<MessagableTab>();
 
     private IconSource iconSource;
 
@@ -23,52 +23,43 @@ public class MessagableTabbedPane extends JTabbedPane implements MessagableTab
     private Icon oldIcon;
     private String oldToolTipText;
 
-    public MessagableTabbedPane()
-    {
+    public MessagableTabbedPane() {
         super();
     }
 
-    public MessagableTabbedPane(int tabPlacement)
-    {
+    public MessagableTabbedPane(int tabPlacement) {
         super(tabPlacement);
     }
 
-    public MessagableTabbedPane(int tabPlacement, int tabPolicy)
-    {
+    public MessagableTabbedPane(int tabPlacement, int tabPolicy) {
         super(tabPlacement, tabPolicy);
     }
 
-    public void insertTab(String title, Icon icon, Component component, String tip, int index)
-    {
+    public void insertTab(String title, Icon icon, Component component, String tip, int index) {
         super.insertTab(title, icon, component, tip, index);
-        messages.add(index, new TabMessages());
+        messagableTabs.add(index, new MessagableTab());
     }
 
-    private Icon getIcon(String severityLabel)
-    {
-        if (iconSource == null)
+    private Icon getIcon(String severityLabel) {
+        if (iconSource == null) {
             iconSource = (IconSource) ApplicationServicesLocator.services().getService(IconSource.class);
+        }
         return iconSource.getIcon("severity." + severityLabel + ".overlay");
     }
 
-    public void setMessage(Object source, Message message, int tabIndex)
-    {
-        TabMessages tabMessages = (TabMessages)this.messages.get(tabIndex);
+    public void setMessage(Object source, Message message, int tabIndex) {
+        MessagableTab messagableTab = this.messagableTabs.get(tabIndex);
         // if first error or less errors than before, update icon/tooltip
-        if (tabMessages.put(source, message))
-        {
-            if (tabMessages.hasErrors())
-            {
+        if (messagableTab.put(source, message)) {
+            if (messagableTab.hasErrors()) {
                 if (!oldHasError) {
                     oldHasError = true;
                     oldIcon = getIconAt(tabIndex);
                     oldToolTipText = getToolTipTextAt(tabIndex);
                 }
                 setIconAt(tabIndex, getIcon(Severity.ERROR.getLabel()));
-                setToolTipTextAt(tabIndex, tabMessages.getFirstMessage());
-            }
-            else
-            {
+                setToolTipTextAt(tabIndex, messagableTab.getFirstMessage());
+            } else {
                 if (oldHasError) {
                     setIconAt(tabIndex, oldIcon);
                     setToolTipTextAt(tabIndex, oldToolTipText);
@@ -80,63 +71,55 @@ public class MessagableTabbedPane extends JTabbedPane implements MessagableTab
         }
     }
 
-    private static class TabMessages
-    {
+    private static class MessagableTab {
         private int numberOfErrors = 0;
-        private HashMap<Object, Message> messages = new HashMap<Object, Message>();
+        private Map<Object, Message> messages = new HashMap<Object, Message>();
         private Stack<Message> messageStack = new Stack<Message>();
 
-        public boolean put(Object key, Message message)
-        {
+        public boolean put(Object key, Message message) {
             boolean firstErrorOrLessErrors = false;
-            Message oldMessage = (Message)this.messages.get(key);
-            if (oldMessage != message)
-            {
-                if ((oldMessage == null) || (message == null) || (oldMessage.getSeverity() != message.getSeverity()))
-                {
-                    if ((message != null) && (message.getSeverity() == Severity.ERROR))
-                    {
-                        if (numberOfErrors == 0)
+            Message oldMessage = this.messages.get(key);
+            if (oldMessage != message) {
+                if ((oldMessage == null) || (message == null) || (oldMessage.getSeverity() != message.getSeverity())) {
+                    if ((message != null) && (message.getSeverity() == Severity.ERROR)) {
+                        if (numberOfErrors == 0) {
                             firstErrorOrLessErrors = true;
-                        ++this.numberOfErrors;
+                        }
+                        this.numberOfErrors++;
                         messageStack.add(message);
-                    }
-                    else if ((oldMessage != null) && (oldMessage.getSeverity() == Severity.ERROR))
-                    {
-                        --this.numberOfErrors;
+                    } else if ((oldMessage != null) && (oldMessage.getSeverity() == Severity.ERROR)) {
+                        this.numberOfErrors--;
                         messageStack.remove(oldMessage);
                         firstErrorOrLessErrors = true;
                     }
                 }
-                if (message != null)
+                if (message != null) {
                     this.messages.put(key, message);
-                else
+                } else {
                     this.messages.remove(key);
+                }
             }
             return firstErrorOrLessErrors;
         }
 
-        public Message get(Object key)
-        {
-            return (Message)this.messages.get(key);
+        public Message get(Object key) {
+            return this.messages.get(key);
         }
 
-        public int getNumberOfErrors()
-        {
+        public int getNumberOfErrors() {
             return this.numberOfErrors;
         }
 
-        public boolean hasErrors()
-        {
+        public boolean hasErrors() {
             return this.numberOfErrors > 0;
         }
 
-        public String getFirstMessage()
-        {
-            if (messageStack.size() > 0)
-                return ((Message)messageStack.firstElement()).getMessage();
-
-            return null;
+        public String getFirstMessage() {
+            if (messageStack.size() > 0) {
+                return messageStack.firstElement().getMessage();
+            } else {
+                return null;
+            }
         }
     }
 }
