bccd5372209276f4060c5a140c9570dd8930f12e
Geoffrey De Smet <ge0ffrey.spam@gmail.com>
2009-10-22 12:31:26 +0000

    ExceptionHanlder throws an exception itself: infinite loop detection for case 2 (see javadocs)
    git-svn-id: https://spring-rich-c.svn.sourceforge.net/svnroot/spring-rich-c/trunk/spring-richclient@2243 817809c7-9d0e-0410-b92d-a7ac8b8adc13

--- spring-richclient-core/src/main/java/org/springframework/richclient/exceptionhandling/AbstractLoggingExceptionHandler.java
+++ spring-richclient-core/src/main/java/org/springframework/richclient/exceptionhandling/AbstractLoggingExceptionHandler.java
@@ -21,6 +21,8 @@ public abstract class AbstractLoggingExceptionHandler extends AbstractRegisterab
     protected LogLevel logLevel = LogLevel.ERROR;
     protected ExceptionPurger exceptionPurger = null;
 
+    private final ThreadLocal<Boolean> alreadyHandlingExceptionOnThisThread = new ThreadLocal<Boolean>();
+
     /**
      * The log level at which the throwable should be logged.
      * The default is ERROR.
@@ -40,21 +42,68 @@ public abstract class AbstractLoggingExceptionHandler extends AbstractRegisterab
 
     /**
      * Logs an exception and shows it to the user.
+     * <p/>
+     * Has infinite loop detection due to exception handling throwing an exception.
+     * An infinite loop can occur in different cases:
+     * <ul>
+     * <li>
+     *   Case 1: The ExceptionHandler throws an exception, which is handled by the ExceptionHandler,
+     *   which throws an exception, ...
+     * </li>
+     * <li>
+     *   Case 2: notifyUserAboutException uses a modal dialog that triggers the event queue to do a focus lost
+     *   That focus lost throws a new exception, catched by the event queue, which delegates it to the ExceptionHandler,
+     *   which still hasn't returned from handling the original exception!
+     *   Then the ExceptionHandler uses a modal dialog again, focus lost, throws exception, ...
+     *   This causes a StackOverflowError or a hang-application-by-starvation due to too many dialogs.</li>
+     * </ul>
      */
     public final void uncaughtException(Thread thread, Throwable throwable) {
-        try {
-            if (exceptionPurger != null) {
-                throwable = exceptionPurger.purge(throwable);
+        Boolean infiniteLoopDetected = alreadyHandlingExceptionOnThisThread.get();
+        if (infiniteLoopDetected != null && infiniteLoopDetected.booleanValue()) {
+            // Preventing infinite loop case 2 (see javadoc)
+            // The original uncaughtException method has not yet returned
+            String detectionLogMessage = "Infinite exception handling loop detected. "
+                    + "The ExceptionHandler has probably thrown the following exception itself:";
+            try {
+                logger.error(detectionLogMessage, throwable);
+            } catch (Throwable ignoredThrowable) {
+                System.err.println(detectionLogMessage);
+                throwable.printStackTrace();
             }
-            logException(thread, throwable);
-            notifyUserAboutException(thread, throwable);
-        } catch (Throwable handlerThrowable) {
-            // An exception handler must never throw a throwable itself
-            // because if it (directly or transitively) handles that throwable it can create an infinite loop
-            System.err.println("The ExceptionHandler handling an exception has thrown the following exception:");
-            handlerThrowable.printStackTrace();
-            // No logging because that could throw an exception
+            // Reset of alreadyHandlingExceptionOnThisThread not needed:
+            // the original uncaughtException method will return and reset it.
+            // Reset not wanted either: even if it's still looping, it is better to be able
+            // to report the first exception and lose performance due to an infinite event loop
+            // than to crash by hang-application-by-starvation due to too many dialogs.
+            return; // Ignore the throwable
         }
+        alreadyHandlingExceptionOnThisThread.set(Boolean.TRUE);
+        try {
+            processUncaughtException(thread, throwable);
+        } catch (Throwable detectionThrowable) {
+            // Preventing infinite loop case 1 (see javadoc)
+            String detectionLogMessage = "The ExceptionHandler has thrown the following exception itself:";
+            try {
+                logger.error(detectionLogMessage, detectionThrowable);
+            } catch (Throwable ignoredThrowable) {
+                System.err.println(detectionLogMessage);
+                detectionThrowable.printStackTrace();
+            }
+        } finally {
+            alreadyHandlingExceptionOnThisThread.set(null);
+        }
+    }
+
+    /**
+     * Logs an exception and shows it to the user.
+     */
+    private void processUncaughtException(Thread thread, Throwable throwable) {
+        if (exceptionPurger != null) {
+            throwable = exceptionPurger.purge(throwable);
+        }
+        logException(thread, throwable);
+        notifyUserAboutException(thread, throwable);
     }
 
     protected String extractErrorCode(Throwable throwable) {
